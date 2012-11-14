f08f7ae6f15bd3586e8d1ce920bc7817a711acd4
Simon Jockers <hello@simonjockers.com>
2012-11-14 16:54:07 +0100

    Remove private token from configuration.

--- Gemfile
+++ Gemfile
@@ -26,3 +26,4 @@ gem "rolify", ">= 3.2.0"
 gem "simple_form", ">= 2.0.4"
 gem "quiet_assets", ">= 1.0.1", :group => :development
 gem "hub", ">= 1.10.2", :require => nil, :group => [:development]
+gem "dotenv", :group => [:development, :test]
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Teikei::Application.config.secret_token = '6933cc3f02f76962602717784d881ea2321dca11ecd6c9bf8d715029d1060100cb4484c53846c06e610eced68833ba01921d015c53c250920a67d62342d02d5b'
+Teikei::Application.config.secret_token = ENV["SECRET_TOKEN"]
