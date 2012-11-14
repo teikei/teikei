0546066c16675cd547003e191633ceb585b4c7fb
Christian Rijke <crijke@gmail.com>
2012-11-14 18:27:48 +0100

    add a mailer spec helper for easier action mailer tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -37,6 +37,7 @@ Spork.prefork do
 
     config.before(:each) do
       DatabaseCleaner.start
+      reset_email
     end
 
     config.after(:each) do
---
+++ spec/support/mailer_helper.rb
@@ -0,0 +1,9 @@
+module MailerHelper
+  def last_email
+    ActionMailer::Base.deliveries.last
+  end
+
+  def reset_email
+    ActionMailer::Base.deliveries = []
+  end
+end
