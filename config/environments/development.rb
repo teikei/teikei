179068ac2793b8c3378edbc4d3cb73e7589572c6
Christian Rijke <crijke@gmail.com>
2012-11-15 16:17:38 +0100

    Enable mail deliveries in development mode.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,7 @@ Teikei::Application.configure do
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   # change to true to allow email to be sent during development
-  config.action_mailer.perform_deliveries = false
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
 
