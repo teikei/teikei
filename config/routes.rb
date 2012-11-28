9f6b7e2328683544ba6e16c110cd4e6038782a6c
Tobias Preuss <tobias.preuss@googlemail.com>
2012-11-28 16:14:29 +0100

    Install and initialize ActiveAdmin.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Teikei::Application.routes.draw do
+  ActiveAdmin.routes(self)
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+
   authenticated :user do
     root :to => 'home#index'
   end
