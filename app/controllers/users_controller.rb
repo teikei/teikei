5841d83cc790053c0a236b9b49c04c9c0b8c38bf
Christian Rijke <crijke@gmail.com>
2012-12-02 23:27:47 +0100

    Use inherited_resources for controllers.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,33 +1,20 @@
-class UsersController < ApplicationController
+class UsersController < InheritedResources::Base
+  respond_to :html, :json
+  actions :index, :show, :update, :destroy
   before_filter :authenticate_user!
 
   def index
     authorize! :index, @user, :message => 'Not authorized as an administrator.'
-    @users = User.all
+    index!
   end
 
-  def show
-    @user = User.find(params[:id])
-  end
-  
   def update
     authorize! :update, @user, :message => 'Not authorized as an administrator.'
-    @user = User.find(params[:id])
-    if @user.update_attributes(params[:user], :as => :admin)
-      redirect_to users_path, :notice => "User updated."
-    else
-      redirect_to users_path, :alert => "Unable to update user."
-    end
+    update!
   end
-    
+
   def destroy
     authorize! :destroy, @user, :message => 'Not authorized as an administrator.'
-    user = User.find(params[:id])
-    unless user == current_user
-      user.destroy
-      redirect_to users_path, :notice => "User deleted."
-    else
-      redirect_to users_path, :notice => "Can't delete yourself."
-    end
+    destroy!
   end
 end
