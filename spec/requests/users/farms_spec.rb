a854cfb7c095e637eaf2451e3ea88fcbd26e7c18
Christian Rijke <crijke@gmail.com>
2012-12-02 18:51:37 +0100

    Add 'farm' feature.

---
+++ spec/requests/users/farms_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper.rb'
+
+feature 'Manage farms', %q{
+  In order to participate as a farmer
+  Aa a registered user
+  I want to be able to manage my farms
+} do
+
+  scenario "User views farms" do
+    pending
+  end
+
+  scenario "User adds a farm" do
+    pending
+  end
+
+  scenario "User changes farm name" do
+    pending
+  end
+
+  scenario "User changes farm location" do
+    pending
+  end
+
+  scenario "User edits farm of another user" do
+    pending
+  end
+
+  scenario "User deletes farm" do
+    pending
+  end
+
+  scenario "User deletes a farm of another user" do
+    pending
+  end
+
+end
