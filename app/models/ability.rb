8eca221f140e31905948120fac8981a411846a56
Christian Rijke <crijke@gmail.com>
2012-12-18 16:13:21 +0100

    Define some initial abilities: Users can read everything and manage farms, Admins can manage everything. Anonymous users can only read farms.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,10 +2,28 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new # guest user (not logged in)
+    user ||= User.new
+
     if user.has_role? :admin
       can :manage, :all
+    elsif user.has_role? :user
+      can :read, :all
+      can :manage, Farm
+    else # guest user
+      can :read, Farm
     end
+
+
+
+    # guest user (not logged in)
+    # if user.has_role? :admin
+    #   can :manage, :all
+    # end
+    # if user.has_role? :user
+    #   can :manage, Farm
+    # else
+    #   can :read, Farm
+    # end
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
