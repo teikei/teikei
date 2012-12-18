4a266139f24b228fac483e8754c83accd25d1859
Christian Rijke <crijke@gmail.com>
2012-12-18 16:02:12 +0100

    Customize the auto-generated seeds file: create roles "user" and "admin" and create a test user for each of these roles.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 puts 'CREATING ROLES'
 Role.create([
-  { :name => 'admin' }, 
-  { :name => 'user' }, 
-  { :name => 'VIP' }
+  { :name => 'admin' },
+  { :name => 'user' }
 ], :without_protection => true)
 puts 'SETTING UP DEFAULT USER LOGIN'
-user = User.create! :name => 'First User', :email => 'user@example.com', :password => 'please', :password_confirmation => 'please'
+user = User.create! :name => 'Default User', :email => 'user@example.com', :password => 'password', :password_confirmation => 'password'
 puts 'New user created: ' << user.name
-user2 = User.create! :name => 'Second User', :email => 'user2@example.com', :password => 'please', :password_confirmation => 'please'
+user2 = User.create! :name => 'Default Admin', :email => 'admin@example.com', :password => 'password', :password_confirmation => 'password'
 puts 'New user created: ' << user2.name
-user.add_role :admin
-user2.add_role :VIP
+user.add_role :user
+user2.add_role :admin
