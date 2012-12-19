cc40b40c21cb03c6ec99a1ac738ec2627369ef64
Simon Jockers <hello@simonjockers.com>
2012-12-19 17:27:21 +0100

    Add a static leaflet map through a Marionette view.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require leaflet
 //= require foundation
 //= require underscore
 //= require backbone
--- app/assets/javascripts/teikei.js
+++ app/assets/javascripts/teikei.js
@@ -8,23 +8,26 @@ Backbone.Marionette.Renderer.render = function(template, data){
 Teikei = new Backbone.Marionette.Application();
 
 Teikei.addRegions({
-  mainRegion: '#main'
+  mainRegion: '#list-container',
+  mapRegion: '#map-container'
 });
 
 Teikei.addInitializer(function(options){
   var placesListView = new Teikei.Views.PlacesList({
     collection: options.places
   });
+  var mapView = new Teikei.Views.Map()
   Teikei.mainRegion.show(placesListView);
+  Teikei.mapRegion.show(mapView);
 });
 
 $(function(){
 
   var cats = new Teikei.Collections.Places([
-    new Teikei.Models.Place({ title: 'John' }),
-    new Teikei.Models.Place({ title: 'Paul' }),
-    new Teikei.Models.Place({ title: 'George' }),
-    new Teikei.Models.Place({ title: 'Ringo' })
+    new Teikei.Models.Place({ location: [52.50, 13.50], title: 'John' }),
+    new Teikei.Models.Place({ location: [52.40, 13.30], title: 'Paul' }),
+    new Teikei.Models.Place({ location: [52.57, 13.40], title: 'George' }),
+    new Teikei.Models.Place({ location: [52.54, 13.56], title: 'Ringo' })
   ]);
 
   Teikei.start({places: cats});
