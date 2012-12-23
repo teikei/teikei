b65f7eb1430305504d9f61574794e91bde7d5406
Christian Rijke <crijke@gmail.com>
2012-12-23 20:41:27 +0100

    Add factories for admins and superadmins to be able to test authorizations.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,24 @@
 FactoryGirl.define do
+
+  # this user has the default role :user
   factory :user do
     name 'Test User'
     email 'example@example.com'
     password 'please'
     password_confirmation 'please'
+
+    factory :admin do
+        after(:create) do |user|
+          user.remove_role(:user)
+          user.add_role(:admin)
+        end
+    end
+
+    factory :superadmin do
+        after(:create) do |user|
+          user.remove_role(:user)
+          user.add_role(:superadmin)
+        end
+    end
   end
 end
