2b548dce9d4e4efcebd93adfbe6365f69bb77d89
Christian Rijke <crijke@gmail.com>
2013-01-19 19:42:58 +0100

    Add seed data for depots.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,6 @@ puts 'DELETE ALL EXISTING FARMS AND USERS'
 Farm.delete_all
 User.delete_all
 
-
 puts 'SETTING UP DEFAULT USER LOGIN'
 user1 = User.create! name: 'First User',
   email: 'first.user@example.com',
@@ -36,9 +35,6 @@ superadmin.add_role :superadmin
 superadmin.save!
 puts "New user created: #{superadmin.name}"
 
-
-
-
 puts 'SETTING UP SOME FARMS'
 farm1 = Farm.create! name: 'User1 farm', location: 'Mexico', lat: 52.50, lng: 13.50
 farm1.user = user1
@@ -50,3 +46,15 @@ farm2.save!
 puts 'New farm created: ' << farm2.name
 farm3 = Farm.create! name: 'Nobody\'s farm', location: 'Death Valley', lat: 52.57, lng: 13.40
 puts 'New farm created: ' << farm3.name
+
+puts 'SETTING UP SOME DEPOTS'
+depot1 = Depot.create! name: 'User1 Depot', location: 'Mexico', lat: 49.50, lng: 13.50
+depot1.user = user1
+depot1.save!
+puts 'New depot created' << depot1.name
+depot2 = Depot.create! name: 'User2 Depot', location: 'Bolivia', lat: 58.40, lng: 13.3
+depot2.user = user2
+depot2.save!
+puts 'New depot created' << depot2.name
+depot3 = Depot.create! name: 'Nobody\'s depot', location: 'Death Valley', lat: 56.57, lng: 13.40
+puts 'New farm created: ' << depot3.name
