c3a462b760e28f572c162d2738cfea2e08e85661
Christian Rijke <crijke@gmail.com>
2013-01-24 02:17:49 +0100

    Add model spec with geocoding spec.

--- spec/models/depot_spec.rb
+++ spec/models/depot_spec.rb
@@ -3,8 +3,14 @@ require 'spec_helper'
 describe Depot do
   before { @depot = build(:depot) }
 
-  subject { @depot }
+  it "should be a valid" do
+    expect(@depot).to be_valid
+  end
 
-  it { should respond_to :name }
-  # it { should respond_to :location }
+  it "geocodes the location when being saved" do
+    @depot.save!
+
+    expect(@depot.lat).not_to be_nil
+    expect(@depot.lng).not_to be_nil
+  end
 end
---
+++ spec/models/place_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe Place do
+  before { @place = build(:place) }
+
+  it "should be a valid" do
+    expect(@place).to be_valid
+  end
+
+  it "geocodes the location when being saved" do
+    @place.save!
+
+    expect(@place.lat).not_to be_nil
+    expect(@place.lng).not_to be_nil
+  end
+end
