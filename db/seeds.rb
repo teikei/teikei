39ef6879c3cb0b5ff9a304947cb4c75cc375a47b
Christian Rijke <crijke@gmail.com>
2013-01-24 01:17:20 +0100

    Remove latitude and longitude from seed script now that we have a geocoder.

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,25 +39,25 @@ superadmin.save!
 puts "New user created: #{superadmin.name}"
 
 puts 'SETTING UP SOME FARMS'
-farm1 = Farm.create! name: 'User1 farm', location: 'Mexico', lat: 52.50, lng: 13.50
+farm1 = Farm.create! name: 'User1 farm', location: 'Mexico'
 farm1.user = user1
 farm1.save!
 puts 'New farm created: ' << farm1.name
-farm2 = Farm.create! name: 'User2 farm', location: 'Bolivia', lat: 52.40, lng: 13.30
+farm2 = Farm.create! name: 'User2 farm', location: 'Bolivia'
 farm2.user = user2
 farm2.save!
 puts 'New farm created: ' << farm2.name
-farm3 = Farm.create! name: 'Nobody\'s farm', location: 'Death Valley', lat: 52.57, lng: 13.40
+farm3 = Farm.create! name: 'Nobody\'s farm', location: 'Death Valley'
 puts 'New farm created: ' << farm3.name
 
 puts 'SETTING UP SOME DEPOTS'
-depot1 = Depot.create! name: 'User1 Depot', location: 'Mexico', lat: 49.50, lng: 13.50
+depot1 = Depot.create! name: 'User1 Depot', location: 'Mexico'
 depot1.user = user1
 depot1.save!
 puts 'New depot created' << depot1.name
-depot2 = Depot.create! name: 'User2 Depot', location: 'Bolivia', lat: 58.40, lng: 13.3
+depot2 = Depot.create! name: 'User2 Depot', location: 'Bolivia'
 depot2.user = user2
 depot2.save!
 puts 'New depot created' << depot2.name
-depot3 = Depot.create! name: 'Nobody\'s depot', location: 'Death Valley', lat: 56.57, lng: 13.40
+depot3 = Depot.create! name: 'Nobody\'s depot', location: 'Death Valley'
 puts 'New farm created: ' << depot3.name
