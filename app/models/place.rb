fe6c592b946a864516609cdd583f48fce79fd9ab
Christian Rijke <crijke@gmail.com>
2013-01-24 01:17:48 +0100

    Add geocoding to places model.

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,10 @@
 class Place < ActiveRecord::Base
+  geocoded_by :location, latitude: :lat, longitude: :lng
+  after_validation :geocode
+
   acts_as_superclass
+
   attr_accessible :location, :name, :lat, :lng, :subtype
   belongs_to :user
+
 end
