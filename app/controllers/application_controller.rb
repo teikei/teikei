7fe9077e778212e39698c913ce0432a50c3cd8f6
Christian Rijke <crijke@gmail.com>
2013-01-26 22:52:32 +0100

    disable CSRF protection as we have our own token authentication mechanism

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
