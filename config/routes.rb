896b8e0a39e421a04b3fdc092026e031f1fc7077
Christian Rijke <crijke@gmail.com>
2013-01-27 17:38:26 +0100

    Use custom session routes for API instead of devise routes (as the controller doesn't inherit from Devise)

--- config/routes.rb
+++ config/routes.rb
@@ -4,17 +4,18 @@ Teikei::Application.routes.draw do
       resources :farms, except: [:new, :edit]
       resources :depots, except: [:new, :edit]
       resources :places, only: [:index]
+      resources :sessions, only: [:create, :destroy]
     end
   end
 
-  devise_for :users, :controllers => { :sessions => "api/v1/sessions" }
-  devise_scope :user do
-    namespace :api do
-      namespace :v1 do
-        resources :sessions, :only => [:create, :destroy]
-      end
-    end
-  end
+  # devise_for :users, :controllers => { :sessions => "api/v1/sessions" }
+  # devise_scope :user do
+  #   namespace :api do
+  #     namespace :v1 do
+  #       resources :sessions, :only => [:create, :destroy]
+  #     end
+  #   end
+  # end
 
   resources :farms
   ActiveAdmin.routes(self)
