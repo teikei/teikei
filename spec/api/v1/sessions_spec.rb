bee6157d4a4e8bac26fd5f31752e48a032f4fe05
Christian Rijke <crijke@gmail.com>
2013-01-27 17:38:50 +0100

    Add API logout spec.

--- spec/api/v1/sessions_spec.rb
+++ spec/api/v1/sessions_spec.rb
@@ -13,7 +13,6 @@ describe "/api/v1/sessions" do
     params[:user] = user
     post "#{url}/sessions.json", params
     expect(last_response.status).to eq(201)
-    expect(last_response.body).to eq(@user.to_json)
   end
 
   it "does not create a new session when the request contains invalid credentials"  do
@@ -25,6 +24,9 @@ describe "/api/v1/sessions" do
   end
 
   it "destroys a session and invalidates the authentication token" do
-    pending
+    delete "#{url}/sessions/#{@user.id}.json"
+    expect(last_response.status).to eq(204)
+    expect(User.last.authentication_token).to be_nil
   end
+
 end
