a285f76d19946d7b86b53655b1ca531638e828d8
Simon Jockers <hello@simonjockers.com>
2013-02-02 14:35:54 +0100

    Add a custom layout with a fullscreen map for the home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < InheritedResources::Base
+  layout 'map.html.haml'
   respond_to :html, :json
   actions :index
   defaults resource_class: User, collection_name: 'users', instance_name: 'user'
