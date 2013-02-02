cd1fca645e5afd2a92a1c0b11ac9056b2df58394
Simon Jockers <hello@simonjockers.com>
2013-02-02 15:44:33 +0100

    Improve seeds for places in order to actually show markers on the map.

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,25 +38,25 @@ superadmin.save!
 puts "New user created: #{superadmin.name}"
 
 puts 'SETTING UP SOME FARMS'
-farm1 = Farm.create! name: 'User1 farm', location: 'Mexico'
+farm1 = Farm.create! name: 'Gutshof', location: 'Fehrbelliner Str. 45a, 16816 Neuruppin'
 farm1.user = user1
 farm1.save!
 puts 'New farm created: ' << farm1.name
-farm2 = Farm.create! name: 'User2 farm', location: 'Bolivia'
+farm2 = Farm.create! name: 'Solidarischer Garten', location: 'Otawistr. 46, 13351 Berlin'
 farm2.user = user2
 farm2.save!
 puts 'New farm created: ' << farm2.name
-farm3 = Farm.create! name: 'Nobody\'s farm', location: 'Death Valley'
+farm3 = Farm.create! name: 'Bioschweinemast Neukoelln', location: 'Karl-Marx-Str. 66, 12043 Berlin'
 puts 'New farm created: ' << farm3.name
 
 puts 'SETTING UP SOME DEPOTS'
-depot1 = Depot.create! name: 'User1 Depot', location: 'Mexico'
+depot1 = Depot.create! name: 'Neukoelln Standort 1', location: 'Richardplatz Berlin'
 depot1.user = user1
 depot1.save!
-puts 'New depot created' << depot1.name
-depot2 = Depot.create! name: 'User2 Depot', location: 'Bolivia'
+puts 'New depot created: ' << depot1.name
+depot2 = Depot.create! name: 'Gaumenfreunde Wedding', location: 'Malplaquetstr. 10 13477 Berlin'
 depot2.user = user2
 depot2.save!
-puts 'New depot created' << depot2.name
-depot3 = Depot.create! name: 'Nobody\'s depot', location: 'Death Valley'
+puts 'New depot created: ' << depot2.name
+depot3 = Depot.create! name: 'Gaumenfreunde Prenzlauer Berg', location: 'Schivelbeiner Str. 6, 10439 Berlin'
 puts 'New farm created: ' << depot3.name
