f96238e8e05437b2c361c7d48bb006d78e169363
Christian Rijke <crijke@gmail.com>
2013-02-10 00:00:10 +0100

    Update actions with a  workaround for the multiple_table_inheritance issue with associations.

--- app/controllers/api/v1/depots_controller.rb
+++ app/controllers/api/v1/depots_controller.rb
@@ -1,6 +1,13 @@
 class Api::V1::DepotsController < Api::V1::BaseController
 
+  def update
+    if params[:places]
+      @depot.places.each { |p| @depot.places.delete(p.place) }
+      @depot.save!
+      @depot.reload
+      @depot.places = Place.find(params[:places]).map(&:place)
     end
+    update!
   end
 
   def create
--- app/controllers/api/v1/farms_controller.rb
+++ app/controllers/api/v1/farms_controller.rb
@@ -1,6 +1,13 @@
 class Api::V1::FarmsController < Api::V1::BaseController
 
+  def update
+    if params[:places]
+      @farm.places.each { |p| @farm.places.delete(p.place) }
+      @farm.save!
+      @farm.reload
+      @farm.places = Place.find(params[:places]).map(&:place)
     end
+    update!
   end
 
   def create
