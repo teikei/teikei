67088dc8c8847a0895c3d74107df54e3bc495941
Simon Jockers <hello@simonjockers.com>
2013-02-12 18:02:26 +0100

    Frontend: Refactor places module – use controller as mediator to decouple the components.

--- app/assets/javascripts/controllers/places_controller.js
+++ app/assets/javascripts/controllers/places_controller.js
@@ -4,9 +4,20 @@ Teikei.module("Places", function(Places, App, Backbone, Marionette, $, _) {
 
     initialize: function(){
       this.collection = new Teikei.Places.Collection();
-      this.mapView = new Teikei.Places.MapView({ collection: this.collection });
-      this.collection.fetch()
+      this.mapView = new Teikei.Places.MapView({
+        collection: this.collection
+      });
+      this.collection.fetch();
+
+      //this.mapView.bind("marker:select", this.openTip);
+
       App.mainRegion.show(this.mapView);
+    },
+
+    openTip: function(marker) {
+      var mapItemView = new Places.MapItemView({model: marker.model});
+      mapItemView.render();
+      marker.bindPopup(mapItemView.el).openPopup();
     }
 
   })
