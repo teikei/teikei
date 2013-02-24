c5ac2d131161ca870c210bc4377c774cedb6aad0
Simon Jockers <hello@simonjockers.com>
2013-02-24 18:41:54 +0100

    Add custom marker icons.

---
+++ app/assets/javascripts/views/places/marker_icon.js
@@ -0,0 +1,24 @@
+Teikei.module("Places.MarkerIcon", function(MarkerIcon, App, Backbone, Marionette, $, _) {
+
+  MarkerIcon.Base = L.Icon.extend({
+    options: {
+      shadowUrl: '/assets/marker-shadow.png',
+      iconSize:     [47, 57],
+      shadowSize:   [64, 68],
+      popupAnchor:  [0, -30]
+    }
+  });
+
+  MarkerIcon.Farm = MarkerIcon.Base.extend({
+    options: {
+      iconUrl: '/assets/marker-farm.png'
+    }
+  });
+
+  MarkerIcon.Depot = MarkerIcon.Base.extend({
+    options: {
+      iconUrl: '/assets/marker-depot.png'
+    }
+  });
+
+});
