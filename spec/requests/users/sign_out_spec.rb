5f1b2ce29579b985396320749421050a45b45c9c
Tobias Preuss <tobias.preuss@googlemail.com>
2013-03-10 22:23:47 +0100

    Localize sign out spec.

--- spec/requests/users/sign_out_spec.rb
+++ spec/requests/users/sign_out_spec.rb
@@ -10,7 +10,7 @@ describe 'Sign Out' do
 
   it 'signs out the current user' do
     sign_out
-    expect(page).to have_content 'Signed out successfully.'
+    expect(page).to have_content I18n.t('devise.sessions.signed_out')
     visit '/'
     expect_user_not_to_be_signed_in
   end
