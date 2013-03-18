1aa9ef12288b3cebaf8c3dfeb88e1879f6f1001f
Christian Rijke <crijke@gmail.com>
2013-03-18 21:34:02 +0100

    add user routes:
    - #signin  sign in
    - #signout sign out

--- app/assets/javascripts/routers/user_router.js
+++ app/assets/javascripts/routers/user_router.js
@@ -3,6 +3,8 @@ Teikei.module("User", function(User, App, Backbone, Marionette, $, _) {
   User.Router = Backbone.Marionette.AppRouter.extend({
 
     appRoutes: {
+      'signin': 'loginPopup',
+      'signout': 'logout'
     },
 
   });
