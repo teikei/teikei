6a6bb297e3152ddeccc98e1ced7588b8b13ca4fd
Christian Rijke <crijke@gmail.com>
2013-03-25 23:03:20 +0100

    use new render syntax (and get rid of annoying deprecation warning).

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < InheritedResources::Base
-  layout 'map.html.haml'
+  layout 'map.html', handler: :haml
   respond_to :html, :json
   actions :index
   defaults resource_class: User, collection_name: 'users', instance_name: 'user'
