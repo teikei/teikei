23b327c7d9d1780e28cb74b18a6c9f5a3065e192
Christian Rijke <crijke@gmail.com>
2013-04-20 00:51:24 +0200

    Add a simple phone number editor in the backend.

--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -126,6 +126,31 @@ describe Place do
     expect(@place).to have(1).error_on(:contact_email)
   end
 
+  it "rejects invalid contact phones" do
+    @place.contact_phone = "foobar 1234"
+    expect(@place).not_to be_valid
+
+    @place.contact_phone = "++ 123 12 321 3123"
+    expect(@place).not_to be_valid
+
+    @place.contact_phone = "123-123-123 foo"
+    expect(@place).not_to be_valid
+  end
+
+  it "accepts valid contact phones" do
+    @place.contact_phone = "+49 12 3123 123 12 3123"
+    expect(@place).to be_valid
+
+    @place.contact_phone = "030 1231-123-123-123"
+    expect(@place).to be_valid
+
+    @place.contact_phone = "121231231231231"
+    expect(@place).to be_valid
+
+    @place.contact_phone = "030/123123 123 123"
+    expect(@place).to be_valid
+  end
+
   it "geocodes the location when being saved" do
     @place.latitude = nil
     @place.longitude = nil
