a85ab3e3b0a4f49a11cef5d44c3f6fdce3e3911e
Tobias Preuss <tobias.preuss@googlemail.com>
2013-04-23 16:42:14 +0200

    Configure mailer host for production environment.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Teikei::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'example.com' }
+  config.action_mailer.default_url_options = { :host => ENV["ACTION_MAILER_HOST_PRODUCTION"] }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
