81ec54d6e93680e5106e151c384b2fb2a0ee83e3
Tobias Preuss <tobias.preuss@googlemail.com>
2013-04-30 17:24:08 +0200

    Add message form to details view of depots and farms.
    + TODO: Test for valid place form data is pending.

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -35,6 +35,25 @@ describe MessagesController do
       end
     end
 
+    context "when sending valid place form data" do
+      it "renders the index page and displays a success message" do
+        pending "Fix recipient address."
+        place = create(:place)
+        post :create, place_form: FactoryGirl.attributes_for(:valid_place_message)
+        expect(response).to redirect_to root_path
+        expect(flash[:notice]).to eq(I18n.t(".controllers.messages.success.email_sent_to", contact_name: place.contact_name))
+      end
+    end
+
+    context "when sending invalid place form data" do
+      it "renders the index page and displays an error message" do
+        place = create(:place)
+        post :create, place_form: FactoryGirl.attributes_for(:invalid_place_message)
+        expect(response).to  redirect_to root_path
+        expect(flash[:notice]).to eq(I18n.t(".controllers.messages.errors.email_not_sent_to", contact_name: place.contact_name))
+      end
+    end
+
   end
 
 end
---
+++ spec/factories/place_message.rb
@@ -0,0 +1,18 @@
+FactoryGirl.define do
+
+  factory :valid_place_message, class: PlaceMessage do
+    places_id 1
+    to "valid-email@example.com"
+    name "John Doe"
+    email "valid-email@example.com"
+    message "This is a valid place message."
+  end
+
+  factory :invalid_place_message, class: PlaceMessage do
+    places_id 1
+    to "valid-email@example.com"
+    email "valid-email@example.com"
+    message "This is an invalid place message."
+  end
+
+end
