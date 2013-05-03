9932b9248c8cc22bbc50698c6df2838014c86c83
Christian Rijke <crijke@gmail.com>
2013-05-03 20:17:43 +0200

    Lock down the entire site.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_filter :authenticate_user!
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
