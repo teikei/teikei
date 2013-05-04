0febe33156b9816e50a5518e33a5b58ed0720dcc
Christian Rijke <crijke@gmail.com>
2013-05-04 13:55:35 +0200

    Remove users controller and use devise implementation instead.

--- app/controllers/users_controller.rb
+++
@@ -1,9 +0,0 @@
-class UsersController < InheritedResources::Base
-  authorize_resource
-  rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_path, alert: exception.message
-  end
-
-  respond_to :html, :json
-  actions :index, :show, :update, :destroy
-end
