c15d08db19b968314fbe460036627bf0b51205be
Tobias Preuss <tobias.preuss@googlemail.com>
2013-05-22 16:28:47 +0200

    Show participate-pop-up for consumers and farmers infos.
    + Enable addressing and switching tabs.

--- app/assets/javascripts/controllers/participate_controller.js
+++ app/assets/javascripts/controllers/participate_controller.js
@@ -8,14 +8,18 @@ Teikei.module("Participate", function(Participate, App, Backbone, Marionette, $,
       App.vent.on("participate:for:citizens", this.showConsumerInfos, this);
       App.vent.on("participate:for:farmers", this.showFarmerInfos, this);
 
+      this.participateView.bind("consumers:tab:click", this.showConsumerInfos, this);
+      this.participateView.bind("farmers:tab:click", this.showFarmerInfos, this);
 
       App.participatePopup.show(this.participateView);
     },
 
     showConsumerInfos: function() {
+      this.participateView.showConsumerInfos();
     },
 
     showFarmerInfos: function() {
+      this.participateView.showFarmerInfos();
     }
 
   });
