bec4090275958abe8ef8730e8bb1267e3f695a54
Tobias Preuss <tobias.preuss@googlemail.com>
2013-05-22 17:26:21 +0200

    Prepare Jasmine test, disabled though.

--- spec/javascripts/participate_spec.js
+++ spec/javascripts/participate_spec.js
@@ -11,6 +11,47 @@ describe("Participate", function() {
     expect(participateController.participateView).toBeInstanceOf(Teikei.Participate.ParticipateView);
   });
 
+  describe("ParticipateView", function() {
+
+    xit("should show consumers infos when a 'participate:for:consumers' is fired.", function() {
+      // bind callback to participateController.participateView
+      // trigger participate:for:consumers
+      // expect callback.showConsumerInfos toHaveBeenCalled()
+    });
+
+    xit("should show farmers infos when a 'participate:for:farmers' is fired.", function() {
+      // bind callback to participateController.participateView
+      // trigger participate:for:farmers
+      // expect callback.showFarmerInfos toHaveBeenCalled()
+    });
+
+    xit("should fire 'consumers:tab:click' when consumers tab is clicked.", function() {
+      // var callback = jasmine.createSpy();
+      // participateController.participateView.bind("consumers:tab:click", callback, this);
+      // $("#consumers-tab").trigger("click");
+      // expect(callback).toHaveBeenCalled();
+    });
+
+    xit("should fire 'farmers:tab:click' when farmers tab is clicked.", function() {
+      // var callback = jasmine.createSpy();
+      // participateController.participateView.bind("farmers:tab:click", callback, this);
+      // $("#farmers-tab").trigger("click");
+      // expect(callback).toHaveBeenCalled();
+    });
+
+    xit("should show consumers infos when a 'consumers:tab:click' is fired.", function() {
+      // bind callback to participateController.participateView
+      // trigger consumers:tab:click
+      // expect callback.showConsumerInfos toHaveBeenCalled()
+    });
+
+    xit("should show farmers infos when a 'farmers:tab:click' is fired.", function() {
+      // bind callback to participateController.participateView
+      // trigger farmers:tab:click
+      // expect callback.showFarmerInfos toHaveBeenCalled()
+    });
+
+  });
 
   describe("ParticipateView", function() {
 
