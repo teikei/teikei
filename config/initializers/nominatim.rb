5bcc8a54b83011060c2530f1708ebaf24f3ccf1f
Christian Rijke <crijke@gmail.com>
2013-08-01 16:42:36 +0200

    Replace generic geocoded gem with nominatim gem.

---
+++ config/initializers/nominatim.rb
@@ -0,0 +1,6 @@
+Nominatim.configure do |config|
+  config.email = 'your-email-address@example.com'
+  config.endpoint = 'http://nominatim.openstreetmap.org'
+  # config.user_agent       = DEFAULT_USER_AGENT
+  # config.accept_language  = DEFAULT_LANGUAGE
+end
