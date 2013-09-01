f012bc7f1047ef12c5ffaccdda95ca95050bdd4e
Tobias Preuss <tobias.preuss@googlemail.com>
2013-09-01 19:57:40 +0200

    Enable error message for unauthorized edits.

--- app/assets/javascripts/views/places/entry.js
+++ app/assets/javascripts/views/places/entry.js
@@ -1,6 +1,6 @@
 Teikei.module("Places", function(Places, App, Backbone, Marionette, $, _) {
 
-  Places.EntryView = Marionette.ItemView.extend({
+  Places.EntryView = Teikei.Base.ItemView.extend({
 
     className: "reveal-modal large",
     template: "places/entry",
@@ -116,6 +116,7 @@ Teikei.module("Places", function(Places, App, Backbone, Marionette, $, _) {
       var errors = this.forms[this.step].validate();
 
       if (errors === null) {
+        this.hideAlertMessage(true);
         _.each(this.forms, function(form) {
           var data = form.getValue();
           model.set(data);
@@ -125,9 +126,16 @@ Teikei.module("Places", function(Places, App, Backbone, Marionette, $, _) {
           success: function(model, response, options) {
             self.collection.add(model);
             self.$el.trigger('reveal:close');
+          },
+          error: function(model, xhr, options) {
+            self.showAuthorizationError(xhr);
           }
         });
       }
+    },
+
+    showAuthorizationError: function(xhr) {
+      this.showError(xhr, "Für diese Aktion fehlen dir die nötigen Rechte.");
     },
 
     clearMapPreview: function() {
