teikei/teikei

commit 82a34238289944dc15a56551719f50c48ec4042c

Add styling for required entry form fields.

app/assets/templates/places/forms/entryDepotBasics.jst.ejs
@@ -2,15 +2,15 @@
 <h3>Schritt 1 von 2</h3>
 <fieldset>
   <legend>Name und Betrieb</legend>
-  <div data-fields="name,places"></div>
+  <div class="required" data-fields="name,places"></div>
 </fieldset>
 
 <fieldset>
   <legend>Adresse der Abholstelle</legend>
   <div id="row">
     <div class="six columns">
-      <div class="address" data-fields="address"></div>
-      <div class="city" data-fields="city"></div>
+      <div class="required" data-fields="address"></div>
+      <div class="required" data-fields="city"></div>
       <a class="small button preview-button">Vorschau</a>
     </div>
     <div class="preview-map six columns">

app/assets/templates/places/forms/entryFarmBasics.jst.ejs
@@ -1,16 +1,16 @@
 <h2 class="headline"></h2>
 <h3>Schritt 1 von 4</h3>
 <fieldset>
-  <div class="twelve columns" data-fields="name"></div>
+  <div class="twelve columns required" data-fields="name"></div>
   <legend>Name und Adresse</legend>
-    <div class="six columns">
-      <div class="address" data-fields="address"></div>
-      <div class="city" data-fields="city"></div>
-      <a class="small button preview-button">Vorschau</a>
-    </div>
-    <div class="preview-map six columns">
-      <img class="preview-image" src="/assets/preview-placeholder.png" />
-      <img src="/assets/marker-farm.png" class="preview-marker leaflet-marker-icon" />
-    </div>
+  <div class="six columns">
+    <div class="required" data-fields="address"></div>
+    <div class="required" data-fields="city"></div>
+    <a class="small button preview-button">Vorschau</a>
+  </div>
+  <div class="preview-map six columns">
+    <img class="preview-image" src="/assets/preview-placeholder.png" />
+    <img src="/assets/marker-farm.png" class="preview-marker leaflet-marker-icon" />
+  </div>
 </fieldset>
 

app/assets/stylesheets/includes/_home.scss
@@ -60,6 +60,13 @@
       display: inline;
     }
 
+    .required {
+      // styling for labels of required form elements
+      label:after {
+        content:"*";
+      }
+    }
+
     .radios,
     .checkboxes {
       ul {

app/assets/templates/places/forms/entryDepotContact.jst.ejs
@@ -2,7 +2,7 @@
 <h3>Schritt 2 von 2</h3>
 <fieldset>
   <legend>Ansprechpartner</legend>
-  <div data-fields="contact_name"></div>
-  <div data-fields="contact_email"></div>
-  <div data-fields="contact_phone"></div>
+  <div class="required" data-fields="contact_name"></div>
+  <div class="required" data-fields="contact_email"></div>
+  <div class="required" data-fields="contact_phone"></div>
 </fieldset>

app/assets/templates/places/forms/entryFarmContact.jst.ejs
@@ -2,9 +2,9 @@
 <h3>Schritt 4 von 4</h3>
 <fieldset>
   <legend>Kontakt</legend>
-  <div data-fields="contact_name"></div>
-  <div data-fields="contact_function"></div>
-  <div data-fields="contact_email"></div>
+  <div class="required" data-fields="contact_name"></div>
+  <div class="required" data-fields="contact_function"></div>
+  <div class="required" data-fields="contact_email"></div>
   <div data-fields="contact_url"></div>
   <div data-fields="contact_phone"></div>
 </fieldset>

app/assets/templates/places/forms/entryFarmDetails.jst.ejs
@@ -2,9 +2,9 @@
 <h3>Schritt 2 von 4</h3>
 <fieldset>
   <legend>Beschreibung</legend>
-  <div data-fields="description"></div>
-  <div data-fields="products" class="checkboxes"></div>
-  <div data-fields="founded_at_year"></div>
+  <div class="required" data-fields="description"></div>
+  <div class="required checkboxes" data-fields="products"></div>
+  <div class="required" data-fields="founded_at_year"></div>
   <div data-fields="founded_at_month"></div>
   <div data-fields="farming_standard"></div>
   <div data-fields="is_solawi_member"></div>

app/assets/templates/places/forms/entryFarmMembership.jst.ejs
@@ -2,8 +2,8 @@
 <h3>Schritt 3 von 4</h3>
 <fieldset>
   <legend>Mitgliedschaft</legend>
-  <div data-fields="accepts_new_members" class="radios"></div>
-  <div data-fields="maximum_members"></div>
-  <div data-fields="participation"></div>
+  <div class="required radios" data-fields="accepts_new_members"></div>
+  <div class="required" data-fields="maximum_members"></div>
+  <div class="required" data-fields="participation"></div>
 </fieldset>
 

commit 0da485310a235f8c18d524df6761ae1e266c5e4b

Bugfix: Geocoding was broken due to detached input fields.
+ Restore required class for address and city input fields.
+ Broken in 82a34238289944dc15a56551719f50c48ec4042c.

app/assets/templates/places/forms/entryDepotBasics.jst.ejs
@@ -10,8 +10,8 @@
   <legend>Adresse der Abholstelle</legend>
   <div id="row">
     <div class="six columns">
-      <div class="required data-block" data-fields="address"></div>
-      <div class="required data-block" data-fields="city"></div>
+      <div class="required data-block address" data-fields="address"></div>
+      <div class="required data-block city" data-fields="city"></div>
       <a class="small button preview-button">Vorschau</a>
     </div>
     <div class="preview-map six columns">

app/assets/templates/places/forms/entryFarmBasics.jst.ejs
@@ -4,8 +4,8 @@
   <div class="required data-block twelve columns" data-fields="name"></div>
   <legend>Name und Adresse</legend>
   <div class="six columns">
-    <div class="required data-block" data-fields="address"></div>
-    <div class="required data-block" data-fields="city"></div>
+    <div class="required data-block address" data-fields="address"></div>
+    <div class="required data-block city" data-fields="city"></div>
     <a class="small button preview-button">Vorschau</a>
   </div>
   <div class="preview-map six columns">

spec/javascripts/places_spec.js
@@ -121,6 +121,11 @@ describe("Places", function() {
       });
     });
 
+    it("contains queryable input fields for address and city", function() {
+      expect(placesController.entryView.ui.addressInput.val()).toBeDefined();
+      expect(placesController.entryView.ui.cityInput.val()).toBeDefined();
+    });
+
     // FIXME: doesn't work as the reveal modal is not really working in this test setup
     // it("should be closed when the containing modal is closed", function() {
     //   runs(function() {