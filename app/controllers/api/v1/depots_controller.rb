01aaa6389e3afde8d709c4a32c3b07624d2b6d12
Tobias Preuss <tobias.preuss@googlemail.com>
2013-09-08 00:57:19 +0200

    Refactor assigning places associations.
    + Use before_filter.

--- app/controllers/api/v1/depots_controller.rb
+++ app/controllers/api/v1/depots_controller.rb
@@ -1,29 +1,45 @@
 class Api::V1::DepotsController < Api::V1::BaseController
-  def update
-    assign_places
-    update!
-  end
+
+  before_filter :assign_places, only: [:create, :update]
 
   def create
-    assign_places
     @depot.user = current_user if current_user
     create!
   end
 
+  def update
+    update!
+  end
+
+
+  private
+
   def assign_places
-    if params[:places].nil?
-      @depot.places = []
+    places_params = params[:places]
+    if places_params
+      replace_all_places_associations(places_params)
     else
-      places_params = params[:places]
-      if places_params
-        place_ids = if places_params[0].is_a? Hash
-                      places_params.map { |p| p[:id].to_i }
-                    else
-                      places_params
-                    end
-        @depot.places = Place.find(place_ids)
-      end
+      remove_all_places_associations
     end
+  end
+
+  def replace_all_places_associations(places_params)
+    place_ids = extract_place_ids(places_params)
+    @depot.places = Place.find(place_ids)
+  end
+
+  def extract_place_ids(places_params)
+    if places_params[0].is_a? Hash
+      return places_params.map { |p| p[:id].to_i }
+    elsif places_params[0].is_a? String # Used by Select2 sending farms ids as a JSON string.
+      return places_params
+    else
+      raise ArgumentError, "Argument 'places' contains #{places_params[0].class} objects."
+    end
+  end
+
+  def remove_all_places_associations
+    @depot.places = []
   end
 
 
