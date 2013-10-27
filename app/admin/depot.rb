6bef329548e87c9f1370ee9587a476667335e58c
Christian Rijke <crijke@gmail.com>
2013-10-27 17:24:22 +0100

    Create customized form and only show editable attributes (e.g. no user).

--- app/admin/depot.rb
+++ app/admin/depot.rb
@@ -1,9 +1,12 @@
 ActiveAdmin.register Depot do
+  form do |f|
+    PlaceForm.form(f)
+    f.buttons
+  end
 
   index do
     column :name
     column :location
     default_actions
   end
-
 end
--- app/admin/farm.rb
+++ app/admin/farm.rb
@@ -1,9 +1,24 @@
 ActiveAdmin.register Farm do
+  form do |f|
+    PlaceForm.form(f)
+    f.inputs "Additional Farm Info" do
+      f.input :founded_at_year
+      f.input :founded_at_month
+      f.input :maximum_members
+      f.input :accepts_new_members
+      f.input :products
+      f.input :participation
+      f.input :acts_ecological
+      f.input :economical_behavior
+      f.input :contact_function
+      f.input :contact_url
+    end
+    f.buttons
+  end
 
   index do
     column :name
     column :location
     default_actions
   end
-
 end
