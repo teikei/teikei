4464571dbaa2516153eec3be0aff3e754e89558b
Tobias Preuss <tobias.preuss@googlemail.com>
2013-10-28 22:58:25 +0100

    Enable German locale for deployment server.

--- config/application.rb
+++ config/application.rb
@@ -53,7 +53,7 @@ module Teikei
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :de
     I18n.locale = :de
 
     # Configure the default encoding used in templates for Ruby 1.9.
