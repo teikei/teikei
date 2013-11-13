0160ab9d4a6ebe44d83f16200f94a77c36c5021a
Tobias Preuss <tobias.preuss@googlemail.com>
2013-11-13 19:27:22 +0100

    Rescue from error caused by invalid recipient address.

--- app/controllers/api/v1/place_messages_controller.rb
+++ app/controllers/api/v1/place_messages_controller.rb
@@ -29,10 +29,18 @@ class Api::V1::PlaceMessagesController < ApplicationController
       message_data["message"] = form_data[:message]
 
       message = PlaceMessage.new(message_data)
-      if message.deliver
-        render json: { message: I18n.t("messages_controller.success.message_sent", recipient: place.contact_name) }, status: 201
-      else
-        render json: { error: I18n.t("messages_controller.errors.message_not_sent", recipient: place.contact_name) }, status: 401
+      begin
+        if message.deliver
+          render json: { message: I18n.t("messages_controller.success.message_sent", recipient: place.contact_name) }, status: 201
+        else
+          render json: { error: I18n.t("messages_controller.errors.message_not_sent", recipient: place.contact_name) }, status: 401
+        end
+      rescue => e
+        if e.is_a?(ArgumentError) && e.message == "An SMTP To address is required to send a message. Set the message smtp_envelope_to, to, cc, or bcc address."
+          render json: { error: I18n.t("messages_controller.errors.invalid_recipient_address", recipient: place.contact_name) }, status: 422
+        else
+          render json: { error: e.message }, status: 500
+        end
       end
     end
 
