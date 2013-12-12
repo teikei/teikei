ef20c46da3bad1c50adbce4dde9c2ca026bea565
Tobias Preuss <tobias.preuss@googlemail.com>
2013-12-13 00:27:26 +0100

    Prepopulate contact name in new place entry form.

---
+++ spec/javascripts/form_helper_spec.js
@@ -0,0 +1,103 @@
+describe("FormHelper", function() {
+
+
+  describe("Prepopulation when currentUser is present and valid", function() {
+
+    beforeEach(function () {
+      Teikei.currentUser = new Teikei.User.Model({
+        name: "John Doe"
+      });
+    });
+
+    it("throws an IllegalParameterException when the place model is undefined", function() {
+      spyOn(Teikei.FormHelper, "prepopulatePlaceModel").andCallThrough();
+      var place = undefined;
+      expect(function() {
+        Teikei.FormHelper.prepopulatePlaceModel(place);
+      }).toThrow("IllegalParameterException: Place model cannot be undefined.");
+    });
+
+    it("prepopulates the contact name with the name of the current user if the place model is new", function() {
+      spyOn(Teikei.FormHelper, "prepopulatePlaceModel").andCallThrough();
+      var place = new Teikei.Places.Model();
+      var prepopulatedPlace = Teikei.FormHelper.prepopulatePlaceModel(place);
+      expect(prepopulatedPlace.get("contact_name")).toEqual(Teikei.currentUser.get("name"));
+    });
+
+    it("prepopulates the contact name with the name of the current user if it is empty", function() {
+      spyOn(Teikei.FormHelper, "prepopulatePlaceModel").andCallThrough();
+      var place = new Teikei.Places.Model({
+        contact_name: ""
+      });
+      var prepopulatedPlace = Teikei.FormHelper.prepopulatePlaceModel(place);
+      expect(prepopulatedPlace.get("contact_name")).toEqual(Teikei.currentUser.get("name"));
+    });
+
+    it("prepopulates the contact name with the name of the current user if it is undefined", function() {
+      spyOn(Teikei.FormHelper, "prepopulatePlaceModel").andCallThrough();
+      var place = new Teikei.Places.Model({
+        contact_name: undefined
+      });
+      var prepopulatedPlace = Teikei.FormHelper.prepopulatePlaceModel(place);
+      expect(prepopulatedPlace.get("contact_name")).toEqual(Teikei.currentUser.get("name"));
+    });
+
+  });
+
+
+
+  describe("Prepopulation when the place model is present and valid", function() {
+
+    var place;
+
+    beforeEach(function () {
+      place = new Teikei.Places.Model({
+        contact_name: "Jane Doe"
+      });
+    });
+
+    it("rejects overwriting an existing contact name in the place model", function() {
+      spyOn(Teikei.FormHelper, "prepopulatePlaceModel").andCallThrough();
+      Teikei.currentUser = new Teikei.User.Model({
+        name: "John Doe"
+      });
+      var prepopulatedPlace = Teikei.FormHelper.prepopulatePlaceModel(place);
+      expect(prepopulatedPlace.get("contact_name")).toEqual(place.get("contact_name"));
+    });
+
+    it("rejects overwriting the contact name in a place model with an empty string", function() {
+      spyOn(Teikei.FormHelper, "prepopulatePlaceModel").andCallThrough();
+      Teikei.currentUser = new Teikei.User.Model({
+        name: ""
+      });
+      var prepopulatedPlace = Teikei.FormHelper.prepopulatePlaceModel(place);
+      expect(prepopulatedPlace.get("contact_name")).toEqual(place.get("contact_name"));
+    });
+
+    it("rejects overwriting the contact name in a place model with undefined", function() {
+      spyOn(Teikei.FormHelper, "prepopulatePlaceModel").andCallThrough();
+      Teikei.currentUser = new Teikei.User.Model({
+        name: undefined
+      });
+      var prepopulatedPlace = Teikei.FormHelper.prepopulatePlaceModel(place);
+      expect(prepopulatedPlace.get("contact_name")).toEqual(place.get("contact_name"));
+    });
+
+    it("rejects overwriting the contact name in a place model when the current user model is new", function() {
+      spyOn(Teikei.FormHelper, "prepopulatePlaceModel").andCallThrough();
+      Teikei.currentUser = new Teikei.User.Model();
+      var prepopulatedPlace = Teikei.FormHelper.prepopulatePlaceModel(place);
+      expect(prepopulatedPlace.get("contact_name")).toEqual(place.get("contact_name"));
+    });
+
+    it("rejects overwriting the contact name in a place model when the current user model is undefined", function() {
+      spyOn(Teikei.FormHelper, "prepopulatePlaceModel").andCallThrough();
+      Teikei.currentUser = undefined;
+      var prepopulatedPlace = Teikei.FormHelper.prepopulatePlaceModel(place);
+      expect(prepopulatedPlace.get("contact_name")).toEqual(place.get("contact_name"));
+    });
+
+  });
+
+
+});
