cc27f6005247c9b6e548bf1375171c8b6cacdad6
Simon Jockers <hello@simonjockers.com>
2014-01-04 12:34:34 +0100

    Refactor modal region for better readability.

--- app/assets/javascripts/views/modal_region.js
+++ app/assets/javascripts/views/modal_region.js
@@ -3,18 +3,11 @@ Teikei.module("Base", function(Base, App, Backbone, Marionette, $, _) {
   Base.ModalRegion = Backbone.Marionette.Region.extend({
     el: "#modal-container",
 
-    constructor: function(){
-      _.bindAll(this, ['showModal']);
-      Backbone.Marionette.Region.prototype.constructor.apply(this, arguments);
-      this.on("show", this.showModal, this);
-    },
-
-    showModal: function(view){
+    onShow: function(view){
       view.$el.reveal({
         closeOnBackgroundClick: false,
         closed: this.close
       });
     }
-
   });
 });
