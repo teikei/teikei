21d7cff52abbb481ae668de94fda0b46d9d57f62
Christian Rijke <crijke@gmail.com>
2014-01-12 03:35:25 +0100

    Force content type text/plain for file upload for IE9 compatibility, don’t use RABL in this case.

--- app/models/image.rb
+++ app/models/image.rb
@@ -4,4 +4,13 @@ class Image < ActiveRecord::Base
   mount_uploader :file, ImageUploader
   attr_accessible :description, :file
   belongs_to :place
+
+  def to_fileupload
+    {
+      id: id,
+      url: file.large.url,
+      thumbnail_url: file.thumbnail.url,
+      description: description
+    }
+  end
 end
