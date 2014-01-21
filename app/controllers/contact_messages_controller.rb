0c952273474fbdffc2e5d97e2f1e5611410beb09
Christian Rijke <crijke@gmail.com>
2014-01-21 22:05:24 +0100

    Use inherited resources to handle validation and send the email message.

--- app/controllers/contact_messages_controller.rb
+++ app/controllers/contact_messages_controller.rb
@@ -1,27 +1,9 @@
-class ContactMessagesController < ApplicationController
+class ContactMessagesController < InheritedResources::Base
+  actions :new, :create
 
   respond_to :html
 
   def create
-    if params.has_key? :contact_form
-      create_contact_message(params[:contact_form])
-    else
-      redirect_to root_path, notice: t(".controllers.messages.errors.missing_form_data")
-    end
+    create!(notice: t(".controllers.messages.success.email_sent"))
   end
-
-
-  private
-
-  def create_contact_message(form_data)
-    message = ContactMessage.new(form_data)
-    if message.deliver
-      flash[:notice] = t(".controllers.messages.success.email_sent")
-      render :index
-    else
-      flash[:notice] = t(".controllers.messages.errors.email_not_sent")
-      render :index
-    end
-  end
-
 end
