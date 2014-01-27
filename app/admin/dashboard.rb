565c0e11f94e2e71b631c51c5d94d46a4852114e
Christian Rijke <crijke@gmail.com>
2014-01-27 21:41:54 +0100

    Add version info to backend.

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,6 +3,9 @@ ActiveAdmin.register_page "Dashboard" do
   menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
 
   content :title => proc{ I18n.t("active_admin.dashboard") } do
+    panel "Teikei" do
+      para "version #{`git describe --tags --abbrev=0`}"
+    end
     columns do
       column do
         panel "Statistics" do
