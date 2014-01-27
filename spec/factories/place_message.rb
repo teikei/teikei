29c620facf08ba93661dbdab8600eca35f6f0b9d
Tobias Preuss <tobias.preuss@googlemail.com>
2014-01-27 01:35:49 +0100

    Fix spec for place message model.

--- spec/factories/place_message.rb
+++ spec/factories/place_message.rb
@@ -1,10 +1,13 @@
 FactoryGirl.define do
 
   factory :place_message, class: PlaceMessage do
+    recipient_name "Valid Recipent"
     recipient_email "valid-email@example.com"
     sender_name "John Doe"
     sender_email "valid-email@example.com"
     message "This is a valid place message."
+    mail_form_path "http://www.example.com/place/23/details"
+    place_name "My little Farm"
   end
 
   factory :valid_place_message, class: PlaceMessage do
