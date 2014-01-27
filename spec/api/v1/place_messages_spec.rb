d1b46794ef4de58dcbbc804fda3457d8842799a3
Tobias Preuss <tobias.preuss@googlemail.com>
2014-01-27 16:33:21 +0100

    Simplify spec for sending place messages.
    + Parameter "place_id" is not a natural member of the "place_form" object.

--- spec/api/v1/place_messages_spec.rb
+++ spec/api/v1/place_messages_spec.rb
@@ -7,23 +7,26 @@ describe "/api/v1/send_message" do
   it "sends valid place form data" do
     place = create(:place)
     params = {}
-    params[:place_form] = FactoryGirl.attributes_for(:valid_place_message)
+    params[:place_form] = FactoryGirl.attributes_for(:place_message)
+    params[:place_form][:place_id] = place.id
     post "#{url}/send_message", params
     expect(last_response.status).to eq(201)
   end
 
-  it "rejects sending invalid place form data" do
+  it "rejects sending incomplete place form data (missing: sender_name)" do
     place = create(:place)
     params = {}
-    params[:place_form] = FactoryGirl.attributes_for(:invalid_place_message)
+    params[:place_form] = FactoryGirl.attributes_for(:place_message)
+    params[:place_form][:place_id] = place.id
+    params[:place_form][:sender_name] = nil
     post "#{url}/send_message", params
     expect_message_not_sent_failure(last_response, place.contact_name)
   end
 
   it "rejects sending place form data containing a non-existing places id" do
-    # Places table is currently empty on purpose.
     params = {}
-    params[:place_form] = FactoryGirl.attributes_for(:valid_place_message)
+    params[:place_form] = FactoryGirl.attributes_for(:place_message)
+    params[:place_form][:place_id] = -1
     post "#{url}/send_message", params
     expect_invalid_recipient_failure(last_response)
   end
--- spec/factories/place_message.rb
+++ spec/factories/place_message.rb
@@ -10,22 +10,4 @@ FactoryGirl.define do
     place_name "My little Farm"
   end
 
-  factory :valid_place_message, class: PlaceMessage do
-    place_id 1
-    recipient_email "valid-email@example.com"
-    recipient_name "Valid Recipent"
-    sender_name "John Doe"
-    sender_email "valid-email@example.com"
-    message "This is a valid place message."
-    mail_form_path "http://www.example.com/place/23/details"
-    place_name "My little Farm"
-  end
-
-  factory :invalid_place_message, class: PlaceMessage do
-    place_id 1
-    recipient_email "valid-email@example.com"
-    sender_email "valid-email@example.com"
-    message "This is an invalid place message."
-  end
-
 end
