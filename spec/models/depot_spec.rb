66d1ce2b55da2ed3926f446a201b479e21efa2b4
Christian Rijke <crijke@gmail.com>
2014-02-01 04:55:40 +0100

    Add multiple owners model specs.

--- spec/models/depot_spec.rb
+++ spec/models/depot_spec.rb
@@ -1,26 +1,31 @@
 require 'spec_helper'
 
 describe Depot do
-  before { @depot = create(:depot) }
+  let(:depot) { create(:depot) }
 
   it "should be a valid" do
-    expect(@depot).to be_valid
+    expect(depot).to be_valid
   end
 
   it "has a single ownership" do
-    expect(@depot.ownerships.length).to eq(1)
+    expect(depot.ownerships.length).to eq(1)
+  end
+
+  it "has multiple ownerships" do
+    depot.users << create(:admin)
+    expect(depot.ownerships.length).to eq(2)
   end
 
   it "insert a depot relation entry" do
     related_depot = build(:depot, name: "A related depot")
-    @depot.places << related_depot
-    expect(@depot.places).to include(related_depot)
+    depot.places << related_depot
+    expect(depot.places).to include(related_depot)
   end
 
   it "inserts a farm relation entry" do
     related_farm = build(:farm, name: "A related farm")
-    @depot.places << related_farm
-    expect(@depot.places).to include(related_farm)
+    depot.places << related_farm
+    expect(depot.places).to include(related_farm)
   end
 
   it "return all aggregated places" do
@@ -32,10 +37,10 @@ describe Depot do
     own_farm.places << [own_farm_depot, own_farm_partner_farm]
     own_farm.save!
 
-    @depot.places << own_farm
-    @depot.save!
+    depot.places << own_farm
+    depot.save!
 
-    aggregated_places = @depot.aggregated_places
+    aggregated_places = depot.aggregated_places
     expect(aggregated_places.size).to eq(3)
     expect(aggregated_places).to include(own_farm)
     expect(aggregated_places).to include(own_farm_depot)
