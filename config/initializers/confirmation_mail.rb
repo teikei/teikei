4aa49986afd68e426f1829cc04bce05cf2c527fe
Tobias Preuss <tobias.preuss@googlemail.com>
2014-02-06 18:36:13 +0100

    Add link to resend confirmation email to alert message.

--- config/initializers/confirmation_mail.rb
+++ config/initializers/confirmation_mail.rb
@@ -1,5 +1,9 @@
 module Devise::Models::Confirmable
 
+  def send_confirmation_instructions
+    ConfirmationMailer.confirmation_email(self).deliver
+  end
+
   def send_on_create_confirmation_instructions
     ConfirmationMailer.confirmation_email(self).deliver
   end
