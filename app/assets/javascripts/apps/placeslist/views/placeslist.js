063d4ee838009f7e36519010dd4cc072ed662eeb
Christian Rijke <crijke@gmail.com>
2014-02-11 21:24:46 +0100

    Consolidate places list view and item view in one file to avoid load order issues.
    Another solution would be an explicit load order in the sprockets manifest.

--- app/assets/javascripts/apps/placeslist/views/placeslist.js
+++ app/assets/javascripts/apps/placeslist/views/placeslist.js
@@ -1,4 +1,69 @@
 Teikei.module("PlacesList", function(PlacesList, Teikei, Backbone, Marionette, $, _) {
+  PlacesList.EntryListItemView = Marionette.ItemView.extend({
+    template: "places/list/entryListItem",
+
+    ui: {
+      previewMap: ".preview-map",
+      previewMarker: ".preview-marker",
+    },
+
+    events: {
+      "click .edit-entry": "editEntry",
+      "click .delete-entry": "deleteEntry"
+    },
+
+    initialize: function(options) {
+      debugger
+      Marionette.ItemView.prototype.initialize.call(this, options);
+      this.model = options.model;
+    },
+
+    editEntry: function(){
+      Teikei.vent.trigger("edit:entry", this.model);
+    },
+
+    deleteEntry: function(){
+      Teikei.vent.trigger("delete:entry", this.model);
+    },
+
+    mapZoomLevel: 14,
+    mapWidth: 600,
+    mapHeight: 240,
+
+    onRender: function() {
+      this.showPreviewTile();
+    },
+
+    showPreviewTile: function() {
+      var source = this.placeholderSource;
+      var lat = this.model.get("latitude");
+      var lng = this.model.get("longitude");
+      var previewMarker = this.ui.previewMarker;
+      var previewMap = this.ui.previewMap;
+      var img = new Image();
+      if (lat && lng) {
+        source = "//api.tiles.mapbox.com/v3/{APIKEY}/{LNG},{LAT},{ZOOM}/{WIDTH}x{HEIGHT}.png"
+        .replace("{APIKEY}", Teikei.Places.MapConfig.APIKEY)
+        .replace("{ZOOM}", this.mapZoomLevel)
+        .replace("{WIDTH}", this.mapWidth)
+        .replace("{HEIGHT}", this.mapHeight)
+        .replace("{LAT}", lat)
+        .replace("{LNG}", lng);
+
+        // only show marker if location is valid
+        img.onload = function() {
+          previewMarker.show();
+          previewMap.css("background-image", "url(" + img.src + ")");
+        };
+
+        img.src = source;
+      }
+    }
+  });
+
+  PlacesList.EntryListEmptyView = Marionette.ItemView.extend({
+    template: "places/list/entryListEmptyView"
+  });
 
   PlacesList.EntryListView = Marionette.CompositeView.extend({
     ui: {
@@ -43,5 +108,4 @@ Teikei.module("PlacesList", function(PlacesList, Teikei, Backbone, Marionette, $
       this.collection = options.collection;
     }
   });
-
 });
--- app/assets/javascripts/apps/placeslist/views/placeslist_empty.js
+++
@@ -1,7 +0,0 @@
-Teikei.module("PlacesList", function(PlacesList, Teikei, Backbone, Marionette, $, _) {
-
-  PlacesList.EntryListEmptyView = Marionette.ItemView.extend({
-    template: "places/list/entryListEmptyView"
-  });
-
-});
