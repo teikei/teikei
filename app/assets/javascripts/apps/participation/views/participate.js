b451bfe2d602318b5a2e6d937c6784b8b217f82f
Christian Rijke <crijke@gmail.com>
2014-02-11 20:55:05 +0100

    Move view events from module controller down to views.

--- app/assets/javascripts/apps/participation/views/participate.js
+++ app/assets/javascripts/apps/participation/views/participate.js
@@ -1,8 +1,7 @@
 Teikei.module("Participate", function(Participate, Teikei, Backbone, Marionette, $, _) {
-
   Participate.ParticipateView = Teikei.Base.ItemView.extend({
-
     className: "reveal-modal large",
+
     template: "participate/participate",
 
     ui: {
@@ -12,25 +11,29 @@ Teikei.module("Participate", function(Participate, Teikei, Backbone, Marionette,
       secondPane: "#info-2"
     },
 
+    events: {
+      "click #info-1-tab": "showInfos1",
+      "click #info-2-tab": "showInfos2",
+    },
+
     triggers: {
-      "click #info-1-tab": "tab:1:click",
-      "click #info-2-tab": "tab:2:click",
-      "click #participate-signup": "signup:click"
+      "click #participate-signup": "show:signup"
     },
 
     showInfos: function(tabNum) {
-      this["_showInfos" + tabNum]();
+      this["showInfos" + tabNum]();
     },
 
-    _showInfos1: function() {
+    showInfos1: function() {
+      Backbone.history.navigate('info/1');
       this.activateTab(this.ui.firstTab, [this.ui.secondTab]);
       this.activatePane(this.ui.firstPane, [this.ui.secondPane]);
     },
 
-    _showInfos2: function() {
+    showInfos2: function() {
+      Backbone.history.navigate('info/2');
       this.activateTab(this.ui.secondTab, [this.ui.firstTab]);
       this.activatePane(this.ui.secondPane, [this.ui.firstPane]);
     }
-
   });
 });
