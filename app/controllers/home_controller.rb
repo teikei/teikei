bd6f6d112e87822d5e836622348d967cf24e6dde
Daniel Mack <zonque@gmail.com>
2014-08-06 20:22:44 +0200

    index: render using TextBlocks

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,11 @@ class HomeController < InheritedResources::Base
   actions :index
   defaults resource_class: User, collection_name: 'users', instance_name: 'user'
 
+  def index
+    @intro_block = TextBlock.block_for('start_intro', I18n.locale)
+    @howitworks_block = TextBlock.block_for('start_howitworks', I18n.locale)
+  end
+
   def map
     @layout_hide_header = true
   end
