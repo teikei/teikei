8c1299d87f473832efbe1fa3b388b0b3239fc869
Christian Rijke <crijke@gmail.com>
2014-09-27 15:22:27 +0200

    Remove some dead code in application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,9 @@
 class ApplicationController < ActionController::Base
-  # rescue_from CanCan::AccessDenied do |exception|
-  #   redirect_to root_path, :alert => exception.message
-  # end
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_path, :alert => exception.message
+  end
 
   layout :choose_layout
-
-  # def auth_token
-  #   request.headers['auth_token']
-  # end
 
   # Method name must match with `config.authentication_method`
   # in `config/initializers/active_admin.rb`
@@ -19,19 +15,13 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  # Path for redirection after user sign-in, depending on role.
-  # Works for Devise sign-in form at least.
-  # def after_sign_in_path_for(user)
-    # (user.has_role? :superadmin) ? admin_dashboard_path : root_path
-  # end
-
   protected
 
   def choose_layout
     if devise_controller?
-      "static"
+      'static'
     else
-      "application"
+      'application'
     end
   end
 end
