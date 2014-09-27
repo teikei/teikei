4503faa01a20628612220e84ba43abdf939c492a
Christian Rijke <crijke@gmail.com>
2014-09-28 00:01:59 +0200

    Convert specs to RSpec 3.1.4 syntax with Transpec
    This conversion is done by Transpec 2.3.7 with the following command:
        transpec
    
    * 6 conversions
        from: obj.should_not
          to: expect(obj).not_to
    
    * 5 conversions
        from: it { should ... }
          to: it { is_expected.to ... }
    
    * 3 conversions
        from: obj.should
          to: expect(obj).to
    
    * 3 conversions
        from: obj.stub(:message)
          to: allow(obj).to receive(:message)
    
    * 1 conversion
        from: obj.should_not_receive(:message)
          to: expect(obj).not_to receive(:message)
    
    * 1 conversion
        from: obj.should_receive(:message)
          to: expect(obj).to receive(:message)
    
    For more details: https://github.com/yujinakayama/transpec#supported-conversions

--- spec/models/faq_spec.rb
+++ spec/models/faq_spec.rb
@@ -2,19 +2,19 @@ require 'spec_helper'
 
 describe Faq, type: :model  do
   it "should be possible to create one" do
-    create(:faq).should be_valid
+    expect(create(:faq)).to be_valid
   end
 
   it "should refuse Faq without question" do
-    Faq.create(question: nil, answer: "Yo.", locale: "en").should_not be_valid
+    expect(Faq.create(question: nil, answer: "Yo.", locale: "en")).not_to be_valid
   end
 
   it "should refuse Faq without answer" do
-    Faq.create(question: "What?", answer: nil, locale: "en").should_not be_valid
+    expect(Faq.create(question: "What?", answer: nil, locale: "en")).not_to be_valid
   end
 
   it "should refuse Faq without locale" do
-    Faq.create(question: "What?", answer: "Yo,", locale: nil).should_not be_valid
+    expect(Faq.create(question: "What?", answer: "Yo,", locale: nil)).not_to be_valid
   end
 
 end
