172143443e1b3c667f0439e6ab384f31fc609425
Simon Jockers <hello@simonjockers.com>
2014-10-22 22:17:13 +0200

    Update styling for overlay views (login and details).

--- app/assets/javascripts/apps/user/views/login.js
+++ app/assets/javascripts/apps/user/views/login.js
@@ -10,8 +10,8 @@ Teikei.module("User", function(User, Teikei, Backbone, Marionette, $, _) {
       signUpForm: "#signup-form",
       signInTab: "#signin-tab",
       signUpTab: "#signup-tab",
-      signInPane: "#signin",
-      signUpPane: "#signup"
+      signInPane: "#signin-content",
+      signUpPane: "#signup-content"
     },
 
     events: {
