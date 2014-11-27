4f89354ddfded66cc264632f900dc9eb0187ffa3
Christian Rijke <crijke@gmail.com>
2014-11-28 00:20:05 +0100

    Enforce ssl in production.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Teikei::Application.config.session_store :cookie_store, key: '_teikei_session'
+Teikei::Application.config.session_store :cookie_store, key: '_teikei_session', secure: Rails.env == 'production'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
