9775328a7bcff9bf493e0e7b59d6ecad3108c720
Simon Jockers <hello@simonjockers.com>
2015-03-12 19:12:06 +0100

    Move foundation js includes to application manifest to make alert close buttons functional (to be fully removed later).

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,30 @@
 // and should be removed as soon as a new solution is in place.
 //= require jquery_ujs
 //= require underscore
+//
+// ---
+//
+// TODO: Remove foundation dependencies with upcoming redesign:
+//
+//= require foundation/modernizr.foundation
+//= require foundation/jquery.placeholder
+//= require foundation/jquery.foundation.alerts
+//= require foundation/jquery.foundation.accordion
+//= require foundation/jquery.foundation.buttons
+//= require foundation/jquery.foundation.tooltips
+//= require foundation/jquery.foundation.forms
+//= require foundation/jquery.foundation.tabs
+//= require foundation/jquery.foundation.navigation
+//= require foundation/jquery.foundation.topbar
+//= require foundation/jquery.foundation.reveal
+//= require foundation/jquery.foundation.orbit
+//= require foundation/jquery.foundation.joyride
+//= require foundation/jquery.foundation.clearing
+//= require foundation/jquery.foundation.mediaQueryToggle
+//= require foundation/app
+//
+// ---
+//
 //= require_tree ./_domscripting
 //= require i18n
 //= require i18n/translations
