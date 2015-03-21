a3f402ec274674386673bf996e1a9b0a1f463012
Christian Rijke <crijke@gmail.com>
2015-03-21 22:20:50 +0100

    Configure action mailer for test environment.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,4 +36,11 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.perform_deliveries = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default charset: "utf-8"
 end
