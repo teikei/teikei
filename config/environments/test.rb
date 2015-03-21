1969df02a639fce102af5772318d202a8fff9829
Christian Rijke <crijke@gmail.com>
2015-03-21 22:38:24 +0100

    Turn off static file server.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -12,8 +12,6 @@ Rails.application.configure do
   # preloads Rails for running tests, you may have to set it to true.
   config.eager_load = false
 
-  # Configure static asset server for tests with Cache-Control for performance.
-  config.serve_static_assets  = true
   config.static_cache_control = 'public, max-age=3600'
 
   # Show full error reports and disable caching.
