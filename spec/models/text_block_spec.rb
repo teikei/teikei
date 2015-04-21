62a52fd349ed805e7335d1c27df530feddb695f3
Christian Rijke <crijke@gmail.com>
2015-04-21 20:51:31 +0200

    Fix specs.

--- spec/models/text_block_spec.rb
+++ spec/models/text_block_spec.rb
@@ -2,25 +2,19 @@ require 'rails_helper'
 
 describe TextBlock, type: :model  do
   it "should be possible to create one" do
-    params = { :name => "foooo", :locale => "de", :body_format => "haml" }
+    params = { :name => "foooo", :locale => "de" }
     t = TextBlock.create(params)
     expect(t).to be_valid
   end
 
   it "should not be possible to create one without locale" do
-    params = { :name => "foooo", :body_format => "haml" }
+    params = { :name => "foooo" }
     t = TextBlock.create(params)
     expect(t).not_to be_valid
   end
 
   it "should not be possible to create one without name" do
-    params = { :locale => "de", :body_format => "haml"  }
-    t = TextBlock.create(params)
-    expect(t).not_to be_valid
-  end
-
-  it "should not be possible to create one without body format" do
-    params = { :locale => "de", :locale => "de" }
+    params = { :locale => "de"  }
     t = TextBlock.create(params)
     expect(t).not_to be_valid
   end
