aaf57d146bb72477c6c7ccb63ca14311c59ea459
Christian Rijke <crijke@gmail.com>
2016-08-06 15:36:37 +0200

    Move express client proxy to client folder, cleanup package.json and remove ruby-gemset.

--- client/index.js
+++ client/index.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const nwb = require('nwb/express')
 const proxy = require('http-proxy-middleware')
+const path = require('path')
+
 const app = express()
 
 app.use(proxy(['**', '!**/app.js', '!/__webpack_hmr'], { target: 'http://localhost:3000' }))
 
 app.use(nwb(express, {
   autoInstall: true,
-  entry: 'client/src/index.js'
+  entry: path.join(__dirname, 'src/index.js'),
+  config: path.join(__dirname, 'nwb.config.js')
 }))
 
 app.listen(8000, 'localhost', (err) => {
@@ -16,6 +19,5 @@ app.listen(8000, 'localhost', (err) => {
     console.error(err.stack)
     process.exit(1)
   }
-
   console.log('server listening at http://localhost:8000')
 })
