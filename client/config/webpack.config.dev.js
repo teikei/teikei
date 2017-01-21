b8156664d6492be3713098b67b1b2db8e4c1d91e
Christian Rijke <crijke@gmail.com>
2017-01-21 17:05:06 +0100

    Don’t fail build on lint errors.

--- client/config/webpack.config.dev.js
+++ client/config/webpack.config.dev.js
@@ -193,6 +193,12 @@ module.exports = {
     // See https://github.com/facebookincubator/create-react-app/issues/186
     new WatchMissingNodeModulesPlugin(paths.appNodeModules)
   ],
+  eslint: {
+    failOnWarning: false,
+    failOnError: false,
+    emitWarning: true,
+    emitError: false
+  },
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
   node: {
