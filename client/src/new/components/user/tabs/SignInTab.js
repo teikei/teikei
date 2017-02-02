d97da0b5a94d59cdc90930bc549ef51606286d3d
Christian Rijke <crijke@gmail.com>
2017-02-02 11:22:27 +0100

    Add login form.

--- client/src/new/components/user/tabs/SignInTab.js
+++ client/src/new/components/user/tabs/SignInTab.js
@@ -1,21 +1,46 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { LocalForm, Control } from 'react-redux-form';
 
-const SignInTab = props => (
-  <li className={`tab-content ${props.active}`} id="signin-content">
-    <p>
-      Melde dich mit deiner Email-Adresse und deinem Passwort an,
-      um Einträge auf der Karte vorzunehmen oder zu bearbeiten.
-    </p>
-    <p>
-      Neu bei <em>Ernte Teilen?</em> Hier geht es zur
-      <a href="#signup" id="signup-link">Registrierung für neue Nutzer</a>
-    </p>
-    <form id="signin-form" className="form-inputs">
-      <input type="submit" className="button" value="Anmelden" />
-      <a href="users/password/new" title="Passwort vergessen?">Passwort vergessen?</a>
-    </form>
-  </li>
-)
+class SignInTab extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { values: { email: '', password: '' } };
+  }
+
+  handleSubmit(values) {
+    console.log(values)
+  }
+
+  render() {
+    return (
+      <li className={`tab-content ${this.props.active}`} id="signin-content">
+        <p>
+          Melde dich mit deiner Email-Adresse und deinem Passwort an,
+          um Einträge auf der Karte vorzunehmen oder zu bearbeiten.
+        </p>
+        <p>
+          Neu bei <em>Ernte Teilen?</em> Hier geht es zur
+          <a href="#signup" id="signup-link">Registrierung für neue Nutzer</a>
+        </p>
+        <div id="signin-form" className="form-inputs">
+          <LocalForm
+            onSubmit={values => this.handleSubmit(values)}
+          >
+            <fieldset>
+              <label htmlFor="signInEmail">E-Mail-Adresse</label>
+              <Control.text model=".email" maxLength="100" />
+              <label htmlFor="signInPassword" maxLength="100">Passwort</label>
+              <Control.text model=".password" type="password" />
+            </fieldset>
+            <input type="submit" className="button" value="Anmelden" />
+            <a href="users/password/new" title="Passwort vergessen?">Passwort vergessen?</a>
+          </LocalForm>
+        </div>
+      </li>
+    )
+  }
+
+}
 
 SignInTab.propTypes = {
   active: React.PropTypes.string.isRequired,
