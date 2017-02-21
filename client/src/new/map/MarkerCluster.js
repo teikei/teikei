dd122f4b89c1422498d2a2efb64fc5247e4ef4dc
Simon Jockers <hello@simonjockers.com>
2017-02-21 13:09:53 +0100

    Marker clustering: Add markup and styling.

--- client/src/new/map/MarkerCluster.js
+++ client/src/new/map/MarkerCluster.js
@@ -1,25 +1,45 @@
-// import React from 'react'
+/* eslint class-methods-use-this: ["error", { "exceptMethods": ["createLeafletElement"] }] */
+
 import React from 'react'
 import ReactDOMServer from 'react-dom/server'
-
 import Leaflet from 'leaflet'
+import { MapLayer } from 'react-leaflet'
 import 'leaflet.markercluster'
 import 'leaflet.markercluster/dist/MarkerCluster.css'
-// import MarkerPopup from './MarkerPopup'
-import { MapLayer } from 'react-leaflet'
+import MarkerClusterIcon from './MarkerClusterIcon'
+
+const BASE_DIAMETER = 70
+const FACTOR = 1.1
 
 function initMarker(place) {
   // const icon = new Places.MarkerIcon[type]()
   const location = [place.latitude, place.longitude]
   const marker = L.marker(location, { place })
-
   return marker
+}
+
+function initClusterIcon(cluster) {
+  const markers = cluster.getAllChildMarkers()
+  const places = markers.map(m => m.options.place)
+  const diameter = (places.length * FACTOR) + BASE_DIAMETER
+
+  const clusterView = Leaflet.divIcon({
+    className: 'cluster',
+    iconSize: Leaflet.point(diameter, diameter),
+    html: ReactDOMServer.renderToStaticMarkup(
+      <MarkerClusterIcon places={places} />,
+    ),
+  })
+  return clusterView
 }
 
 class MarkerCluster extends MapLayer {
 
   createLeafletElement() {
-    return Leaflet.markerClusterGroup()
+    return Leaflet.markerClusterGroup({
+      maxClusterRadius: 50,
+      iconCreateFunction: initClusterIcon,
+    })
   }
 
   updateLeafletElement(fromProps, toProps) {
@@ -31,6 +51,5 @@ class MarkerCluster extends MapLayer {
     }
   }
 }
-
 
 export default MarkerCluster
---
+++ client/src/new/map/MarkerClusterIcon.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import _ from 'underscore'
+
+const countByType = places => _.chain(places)
+  .groupBy(place => place.type.toLowerCase())
+  .map(({ length }, type) => ({ type, count: length }))
+  .value()
+
+const renderIcons = ({ type, count }) => (
+  <div className={`cluster-item ${type}`} key={type}>
+    <span className="cluster-value">{count}</span>
+    <span className="cluster-icon">
+      <img src={`/assets/icon-${type}.svg`} alt={type} />
+    </span>
+  </div>
+)
+
+renderIcons.propTypes = {
+  type: React.PropTypes.string.isRequired,
+  count: React.PropTypes.number.isRequired,
+}
+
+const MarkerClusterIcon = ({ places }) => (
+  <div className="cluster-content">
+    <div className="cluster-wrapper-outer">
+      <div className="cluster-wrapper-inner">
+        { countByType(places).map(renderIcons) }
+      </div>
+    </div>
+  </div>
+)
+
+MarkerClusterIcon.propTypes = {
+  places: React.PropTypes.arrayOf(React.PropTypes.object).isRequired,
+}
+
+export default MarkerClusterIcon
