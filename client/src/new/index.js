666227a06fcd383305f6eec414af5c98cc9eb770
Christian Rijke <crijke@gmail.com>
2017-03-02 20:58:37 +0100

    Add todo for initialization.

--- client/src/new/index.js
+++ client/src/new/index.js
@@ -7,7 +7,19 @@ import 'react-s-alert/dist/s-alert-css-effects/stackslide.css'
 import App from './App'
 import '../app'
 
+
+// TODO add this to global context (window)
+// to call it from the html page and initialize config
+// with the passed parameters
+//
+// export default (config) => {
+//   ReactDOM.render(
+//     <App />,
+//     document.getElementById('main'),
+//   )
+// }
+
 ReactDOM.render(
   <App />,
   document.getElementById('main'),
-);
+)
