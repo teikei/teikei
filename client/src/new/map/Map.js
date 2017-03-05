945daee03671e882d17d0024835a26c245fe20cd
Simon Jockers <hello@simonjockers.com>
2017-03-05 10:31:32 +0100

    Add search field.

--- client/src/new/map/Map.js
+++ client/src/new/map/Map.js
@@ -2,9 +2,15 @@ import React from 'react'
 import { Map, TileLayer } from 'react-leaflet'
 import NavigationContainer from '../navigation/NavigationContainer'
 import MarkerCluster from './MarkerCluster'
+import Search from './Search'
 
 const MapComponent = ({ places, position, zoom, apiKey }) => (
   <div className="map-container">
+    <div className="container">
+      <div className="custom-controls">
+        <Search defaultValue="Ort, Hof oder Initiative" />
+      </div>
+    </div>
     <Map center={position} zoom={zoom.min} className="map">
       <TileLayer
         url={`//{s}.tiles.mapbox.com/v3/${apiKey}/{z}/{x}/{y}.png`}
