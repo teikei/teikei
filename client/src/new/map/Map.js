4263a11e8e4a765bc77d22a11b981566bc3ace19
Simon Jockers <hello@simonjockers.com>
2017-03-05 10:45:46 +0100

    Add PropTypes for MapComponent.

--- client/src/new/map/Map.js
+++ client/src/new/map/Map.js
@@ -8,7 +8,10 @@ const MapComponent = ({ places, position, zoom, apiKey }) => (
   <div className="map-container">
     <div className="container">
       <div className="custom-controls">
-        <Search defaultValue="Ort, Hof oder Initiative" />
+        <Search
+          defaultValue="Ort, Hof oder Initiative"
+          onSelect={newPosition => console.log('TODO: zoom to', newPosition)}
+        />
       </div>
     </div>
     <Map center={position} zoom={zoom.min} className="map">
@@ -24,6 +27,17 @@ const MapComponent = ({ places, position, zoom, apiKey }) => (
 
 MapComponent.propTypes = {
   places: React.PropTypes.arrayOf(React.PropTypes.object).isRequired,
+  position: React.PropTypes.shape({
+    lat: React.PropTypes.number,
+    lon: React.PropTypes.number,
+  }).isRequired,
+  zoom: React.PropTypes.shape({
+    default: React.PropTypes.number,
+    min: React.PropTypes.number,
+    max: React.PropTypes.number,
+    searchResult: React.PropTypes.number,
+  }).isRequired,
+  apiKey: React.PropTypes.string.isRequired,
 }
 
 export default MapComponent
