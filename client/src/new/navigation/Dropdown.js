963590bd1aa8bfe841a503d233026362554d6487
Simon Jockers <hello@simonjockers.com>
2017-03-13 15:48:58 +0100

    Nav: Responsive styling for dropdowns.

--- client/src/new/navigation/Dropdown.js
+++ client/src/new/navigation/Dropdown.js
@@ -1,9 +1,16 @@
+/* eslint no-use-before-define: ["error", { "functions": false }] */
+
 import React from 'react'
 import onClickOutside from 'react-onclickoutside'
 import classNames from 'classnames'
 
-const DropdownMenu = component => (
+const DropdownMenu = (component, onCloseClick) => (
   <div className="dropdown-menu">
+    <button
+      className="dropdown-menu-backdrop"
+      onClick={onCloseClick}
+      aria-label="Close"
+    />
     { component }
   </div>
 )
@@ -29,11 +36,12 @@ class Dropdown extends React.Component {
   }
 
   render = () => (
-    <div className={this.className}>
+     /* eslint-disable jsx-a11y/no-static-element-interactions */
+    <div className={this.className} onClick={this.toggleMenu}>
       <button className={this.props.labelClassName} onClick={this.toggleMenu}>
         {this.props.label}
       </button>
-      { this.state.isActive && DropdownMenu(this.props.menuComponent) }
+      { this.state.isActive && DropdownMenu(this.props.menuComponent, this.toggleMenu) }
     </div>
   )
 }
