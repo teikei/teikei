f787489ce7b545a5473b392860782bb140f17605
Simon Jockers <hello@simonjockers.com>
2017-03-27 12:44:13 +0200

    User info: Code cleanup.

--- client/src/new/editors/UserInfo.js
+++ client/src/new/editors/UserInfo.js
@@ -2,28 +2,26 @@ import React from 'react'
 import { Link } from 'react-router'
 import { EDIT_USER_ACCOUNT } from '../AppRouter'
 
-const UserInfo = ({ user }) => {
-  console.log('user?', user.name, user.email)
-  return (
-    <fieldset className="entries-editor-user-info">
-      <legend>Kontaktdaten</legend>
-      <label>Deine Kontaktdaten</label>
-      <dl className="entries-editor-listing">
-        <dt>Name:</dt>
-        <dd>{user.name}</dd>
-        <dt>Email:</dt>
-        <dd>{user.email}</dd>
-        <dt>Telefon:</dt>
-        <dd>{user.phone || '–'}</dd>
-      </dl>
-      <p className="entries-editor-explanation">
-        <Link target="_blank" to={EDIT_USER_ACCOUNT}>
-          Kontaktdaten ändern
-        </Link>
-      </p>
-    </fieldset>
-  )
-}
+const UserInfo = ({ user }) => (
+  <fieldset className="entries-editor-user-info">
+    <legend>Kontaktdaten</legend>
+    <label htmlFor="contact-data">Deine Kontaktdaten</label>
+    <dl id="contact-data" className="entries-editor-listing">
+      <dt>Name:</dt>
+      <dd>{user.name}</dd>
+      <dt>Email:</dt>
+      <dd>{user.email}</dd>
+      <dt>Telefon:</dt>
+      <dd>{user.phone || '–'}</dd>
+    </dl>
+    <p className="entries-editor-explanation">
+      <Link target="_blank" to={EDIT_USER_ACCOUNT}>
+        Kontaktdaten ändern
+      </Link>
+    </p>
+  </fieldset>
+)
+
 
 UserInfo.propTypes = {
   user: React.PropTypes.shape({
