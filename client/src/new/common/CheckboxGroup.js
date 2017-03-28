302bddfc33bbf6493ace4d2e45abcac6363f6005
Simon Jockers <hello@simonjockers.com>
2017-03-29 01:02:14 +0200

    Checkbox groups: Update markup to use existing CSS rules.

--- client/src/new/common/CheckboxGroup.js
+++ client/src/new/common/CheckboxGroup.js
@@ -2,31 +2,33 @@ import React, { PropTypes } from 'react'
 
 const CheckboxGroup = ({ meta: { touched, error, warning }, input, groupLabel, options }) => (
   <div>
-    <strong>{groupLabel}</strong>
-    <div>
+    <label htmlFor={groupLabel}>{groupLabel}</label>
+    <ul id={groupLabel} className="form-checkbox-group">
       {options.map(({ name, label }, index) => (
-        <label key={`label_${name}`} htmlFor={`${name}[${index}]`}>
-          <input
-            type="checkbox"
-            key={`checkbox_${name}`}
-            name={`${name}[${index}]`}
-            value={name}
-            checked={input.value.indexOf(name) !== -1}
-            onChange={(event) => {
-              const newValue = [...input.value];
-              if (event.target.checked) {
-                newValue.push(name);
-              } else {
-                newValue.splice(newValue.indexOf(name), 1);
-              }
-              return input.onChange(newValue);
-            }}
-          />
-          {label}
-        </label>
+        <li key={`checkbox_${name}`}>
+          <label key={`label_${name}`} htmlFor={`${name}[${index}]`}>
+            <input
+              type="checkbox"
+              name={`${name}[${index}]`}
+              id={`${name}[${index}]`}
+              value={name}
+              checked={input.value.indexOf(name) !== -1}
+              onChange={(event) => {
+                const newValue = [...input.value];
+                if (event.target.checked) {
+                  newValue.push(name);
+                } else {
+                  newValue.splice(newValue.indexOf(name), 1);
+                }
+                return input.onChange(newValue);
+              }}
+            />
+            {label}
+          </label>
+        </li>
       ))}
       {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
-    </div>
+    </ul>
   </div>
 )
 
