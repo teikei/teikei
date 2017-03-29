7a450b301643063537e0749e934520bdedd1f23e
Christian Rijke <crijke@gmail.com>
2017-03-29 22:42:20 +0200

    Navigate back to map with special query parameter after registration.

--- app/controllers/api/v1/confirmations_controller.rb
+++ app/controllers/api/v1/confirmations_controller.rb
@@ -6,9 +6,20 @@ class Api::V1::ConfirmationsController < Devise::ConfirmationsController
     if resource.errors.empty?
       set_flash_message(:notice, :confirmed) if is_flashing_format?
       sign_in(resource) # <= THIS LINE ADDED
-      respond_with_navigational(resource){ redirect_to after_confirmation_path_for(resource_name, resource) }
+      respond_with_navigational(resource){ redirect_to map_url(resource) }
     else
       respond_with_navigational(resource.errors, :status => :unprocessable_entity){ render :new }
     end
   end
+
+  private
+
+  def map_url(user)
+    if user.origin.include? 'solawi'
+      user.origin + '/vernetzungsplatform#/?after_confirmation=true'
+    else
+     user.origin + '/map#/?after_confirmation=true'
+    end
+  end
+
 end
