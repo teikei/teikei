b4a79f8efffde88d82bf00d0946e1d66d757095b
Simon Jockers <hello@simonjockers.com>
2017-04-17 20:55:16 +0200

    Entries list: Move UI strings to translation fils.

--- client/src/myentries/MyEntriesList.js
+++ client/src/myentries/MyEntriesList.js
@@ -2,10 +2,11 @@ import React, { PropTypes } from 'react'
 import { Link } from 'react-router'
 import MyEntriesListItem from './MyEntriesListItem'
 import { NEW_DEPOT, NEW_FARM, NEW_INITIATIVE } from '../AppRouter'
+import i18n from '../i18n'
 
 const placesList = (places) => {
   if (places.length === 0) {
-    return (<div>Keine Einträge vorhanden.</div>)
+    return (<div>{i18n.t('entries.no_entries')}</div>)
   }
   return places.map(p =>
     <MyEntriesListItem key={p.id} place={p} />,
@@ -15,16 +16,16 @@ const placesList = (places) => {
 const MyEntriesList = ({ places }) => (
   <div className="entries-editor-container">
     <section className="entries-list">
-      <h1 className="title">Meine Einträge</h1>
+      <h1 className="title">{i18n.t('entries.my_entries')}</h1>
       <ul className="entries-list-controls">
         <li>
-          <Link to={NEW_DEPOT}>Abholstelle hinzufügen</Link>
+          <Link to={NEW_DEPOT}>{i18n.t('entries.new_depot')}</Link>
         </li>
         <li>
-          <Link to={NEW_FARM}>Betrieb hinzufügen</Link>
+          <Link to={NEW_FARM}>{i18n.t('entries.new_farm')}</Link>
         </li>
         <li>
-          <Link to={NEW_INITIATIVE}>Initiative hinzufügen</Link>
+          <Link to={NEW_INITIATIVE}>{i18n.t('entries.new_initiative')}</Link>
         </li>
       </ul>
       {placesList(places)}
