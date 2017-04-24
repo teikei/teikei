0faa3ec5053bdecdceee8c0b38cdbb38d8b97109
Simon Jockers <hello@simonjockers.com>
2017-04-24 11:29:55 +0200

    Map footer: Make links open in new tabs.

--- client/src/map/MapFooter.js
+++ client/src/map/MapFooter.js
@@ -1,22 +1,22 @@
 import React from 'react'
 import i18n from '../i18n'
 
-const MapFooter = props => (
+const MapFooter = () => (
   <footer className="map-footer">
 
     <ul>
       <li className="map-footer-home">
-        <a href="http://ernte-teilen.org/">
+        <a href="http://ernte-teilen.org/" target="_blank" rel="noopener noreferrer">
           ernte-teilen.org
         </a>
       </li>
       <li className="map-footer-terms">
-        <a href="http://ernte-teilen.org/terms/">
+        <a href="http://ernte-teilen.org/terms/" target="_blank" rel="noopener noreferrer">
           { i18n.t('nav.terms') }
         </a>
       </li>
       <li className="map-footer-imprint">
-        <a href="http://ernte-teilen.org/contact/">
+        <a href="http://ernte-teilen.org/contact/" target="_blank" rel="noopener noreferrer">
           { i18n.t('nav.imprint') }
         </a>
       </li>
@@ -24,11 +24,11 @@ const MapFooter = props => (
 
     { i18n.t('nav.map_data') }
 
-    <a href="https://www.mapbox.com/about/maps/">
+    <a href="https://www.mapbox.com/about/maps/" target="_blank" rel="noopener noreferrer">
       ©&nbsp;MapBox
     </a>
       &nbsp;|&nbsp;
-    <a href="http://www.openstreetmap.org/about/">
+    <a href="http://www.openstreetmap.org/about/" target="_blank" rel="noopener noreferrer">
       ©&nbsp;OpenStreetMap
     </a>
 
