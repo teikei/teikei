64a0e96dca3406f7aaa4df333dbfaa291dec3b4f
Christian Rijke <crijke@gmail.com>
2017-11-13 01:57:45 +0100

    Capistrano flow hook.

--- server/config/deploy.rb
+++ server/config/deploy.rb
@@ -23,5 +23,5 @@ namespace :uploads do
   end
 end
 
-before 'deploy:symlink:linked_files', 'uploads:mv_rails_app_dir'
+before 'deploy:started', 'uploads:mv_rails_app_dir'
 before 'deploy:updated', 'build_client'
