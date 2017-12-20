a8fe2eaaba2b40470ab04ce40e04020a6f18b851
Christian Rijke <crijke@gmail.com>
2017-12-20 22:27:56 +0100

    Remove catch-all error handling.

--- server/app/controllers/api/v1/base_controller.rb
+++ server/app/controllers/api/v1/base_controller.rb
@@ -3,10 +3,6 @@ class Api::V1::BaseController < InheritedResources::Base
 
   respond_to :json, except: [:new, :edit]
 
-  rescue_from StandardError do |exception|
-    render json: { error: exception.message }, status: 500
-  end
-
   rescue_from ActiveRecord::RecordNotFound do |exception|
     render json: { error: exception.message }, status: 404
   end
