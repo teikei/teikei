ef776446aeb16ac114301fdd034f9f1ceb0b3340
Christian Rijke <info@christianrijke.de>
2018-06-08 14:29:34 +0200

    Fix:  Use correct env variable for devise sender email and disclaimer.

--- server/config/initializers/devise.rb
+++ server/config/initializers/devise.rb
@@ -9,7 +9,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = ENV['EMAIL_SENDER_ADDRESS']
+  config.mailer_sender = ENV['SENDER_EMAIL']
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
