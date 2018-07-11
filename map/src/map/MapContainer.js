23c61c7a56dce3bb4cfc633d6b33e345a50d19e9
Christian Rijke <info@christianrijke.de>
2018-07-11 19:28:09 +0200

    TeikeiNext: Initial implementation of focus marker

--- map/src/map/MapContainer.js
+++ map/src/map/MapContainer.js
@@ -6,7 +6,9 @@ const mapStateToProps = ({ map, details }) => ({
   places: map.places,
   data: map.data,
   highlight: map.highlight,
-  position: map.position,
+  position: details.place
+    ? { lat: details.place.latitude, lon: details.place.longitude  - 10 }
+    : map.position,
   padding: config.padding,
   currentPlace: details.place || {},
   zoom: map.zoom || config.zoom.default,
@@ -18,9 +20,6 @@ const mapStateToProps = ({ map, details }) => ({
 
 const mapDispatchToProps = () => ({})
 
-const MapContainer = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(MapComponent)
+const MapContainer = connect(mapStateToProps, mapDispatchToProps)(MapComponent)
 
 export default MapContainer
