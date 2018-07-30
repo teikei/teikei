4fa1921c861601079010fc6cd8445905146fb083
Christian Rijke <info@christianrijke.de>
2018-07-30 15:00:16 +0200

    SnakeCase: Enable snake case mapping in ORM.

--- api/src/app/models/base.js
+++ api/src/app/models/base.js
@@ -1,9 +1,8 @@
-import { Model /* snakeCaseMappers */ } from 'objection'
+import { Model, snakeCaseMappers } from 'objection'
 import { toGeoJSON } from '../util/jsonUtils'
 
 export class BaseModel extends Model {
-  // TODO enable this after migrating frontend to camel case properties
-  // static columnNameMappers = snakeCaseMappers()
+  static columnNameMappers = snakeCaseMappers()
 }
 
 export class EntryBaseModel extends BaseModel {
