15409ed1dd898feb7bd4536af6f4f88e054f8183
Christian Rijke <info@christianrijke.de>
2018-08-08 14:19:15 +0200

    DB: move camel-case conversion to knex level (instead of objection models) so it works consistently for read and write.

--- api/src/app/db.js
+++ api/src/app/db.js
@@ -1,9 +1,15 @@
 import knex from 'knex'
+import Client from 'knex/lib/dialects/postgres'
 import { parse } from 'pg-connection-string'
-import { Model } from 'objection'
+import { Model, knexSnakeCaseMappers } from 'objection'
 
 export default app => {
   const connection = parse(app.get('postgres').url)
-  const db = knex({ connection, client: 'pg' })
+
+  const db = knex({
+    client: Client,
+    connection,
+    ...knexSnakeCaseMappers()
+  });
   Model.knex(db)
 }
