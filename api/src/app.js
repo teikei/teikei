1908a78ff74dbc155eedbef61f01d19a25148d05
Christian Rijke <info@christianrijke.de>
2018-10-04 20:03:06 +0200

    Revert "Deployment: Configure CORS restriction."
    This reverts commit 1e0f65acf9d380171385085cd27db70cf4f09a0c.

--- api/src/app.js
+++ api/src/app.js
@@ -1,6 +1,7 @@
 import path from 'path'
 import favicon from 'serve-favicon'
 import compress from 'compression'
+import cors from 'cors'
 import helmet from 'helmet'
 import dotenv from 'dotenv'
 import feathers from '@feathersjs/feathers'
@@ -12,7 +13,6 @@ import { hooks as authHooks } from '@feathersjs/authentication/lib'
 
 import db from './db'
 import middleware from './middleware'
-import cors from './middleware/cors'
 import logger, { loggerHook } from './hooks/logger'
 import { authorize } from './hooks/authorization'
 import services from './services'
@@ -28,14 +28,13 @@ app.configure(envHelpers())
 const conf = configuration()
 app.configure(conf)
 app.info(conf(), 'App configuration')
-app.use()
+app.use(cors())
 app.use(helmet())
 app.use(compress())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.configure(middleware)
-app.configure(cors)
 app.configure(db)
 app.configure(services)
 if (app.get('enableJobQueues')) {
