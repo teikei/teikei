5d0776ddc539725f7bbe87d1a6aaf9115800fbc5
Christian Rijke <hello@christianrijke.de>
2019-02-27 20:25:01 +0100

    Bug: Exclude inactive entries from search index

---
+++ api/db/migrations/20190227201850_remove_inactive_entries_from_search.js
@@ -0,0 +1,30 @@
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.raw(`
+    drop materialized view entries_search;
+    `),
+    knex.raw(`
+    create materialized view entries_search as
+    select id, 'farm' as type, name, to_tsvector(name) as search 
+    from farms 
+    where farms.active = true 
+    union 
+    select id, 'initiative' as type, name, to_tsvector(name) as search 
+    from initiatives
+    where initiatives.active = true 
+    union 
+    select id, 'depot' as type, name, to_tsvector(name) 
+    from depots
+    where depots.active = true ;
+    `),
+    knex.raw(`
+    create index idx_search on entries_search using GIN(search);`),
+    knex.raw(`
+    create unique index entries_search_id_type on entries_search(id, type)
+    `)
+  ])
+}
+
+exports.down = function(knex, Promise) {
+  return Promise.all([knex.raw('drop materialized view entries_search')])
+}
