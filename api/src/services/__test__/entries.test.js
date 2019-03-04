259c30196f5d11dcdbbeb7d425fc51fc4e7b2208
Christian Rijke <hello@christianrijke.de>
2019-03-04 16:52:05 +0100

    Revert "Testing: Add failing test to see if travis detects it"
    This reverts commit f11ab903e8760407914c0c14bf57246b34e49c44.

--- api/src/services/__test__/entries.test.js
+++ api/src/services/__test__/entries.test.js
@@ -62,10 +62,6 @@ describe('entries service', () => {
     expect(service.remove).toEqual(undefined)
   })
 
-  it('fails', () => {
-    expect(false).toBe(true)
-  })
-
   afterEach(async done => {
     await truncateTestDatabase()
     done()
