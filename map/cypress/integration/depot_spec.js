82779e4e8ddeca50cf5116e56e35dc2cb8a0fc1c
Christian Rijke <hello@christianrijke.de>
2019-03-05 12:48:21 +0100

    Testing: Setup cypress and add test for 'log in and create depot'

---
+++ map/cypress/integration/depot_spec.js
@@ -0,0 +1,48 @@
+describe('Admin User', () => {
+  it('logs in', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('.account-nav-login').click()
+    cy.get('[name="email"]').type('admin@example.com')
+    cy.get('[name="password"]').type('admin')
+    cy.get('[type="submit"]').click()
+    cy.contains('Hallo Teikei Admin, Du hast Dich erfolgreich angemeldet.')
+  })
+
+  const deliveryDaysText = 'Monday, Tuesday, Wednesday'
+  const descriptionText = 'Depot description: This is the description'
+  const urlText = 'http://ernte-teilen.org'
+  const nameText = 'Cypress TestDepot'
+
+  it('creates a new depot', () => {
+    cy.get('.entries-nav-toggle').click()
+    cy.get('.entries-nav-depot').click()
+    cy.get('[name="name"]').type(nameText)
+    cy.get('[name="url"]').type(urlText)
+    cy.get('.Select-placeholder').click()
+    cy.get('.Select-menu-outer').click()
+
+    cy.get('[name="geocoder"]')
+      .type('Alexanderplatz, Berlin')
+      .click()
+      .wait(1000)
+      .type('{downarrow}{enter}')
+    cy.get('[name="description"]').type(descriptionText)
+    cy.get('[name="deliveryDays"]').type(deliveryDaysText)
+    cy.get('.submit').click()
+    cy.contains('wurde erfolgreich gespeichert.')
+  })
+
+  it('searches for the newly created depot', () => {
+    cy.wait(5000)
+      .get('.search-input')
+      .type('Cypress TestDepot', { delay: 100 })
+      .wait(2000)
+      .type('{downarrow}{enter}')
+
+    cy.get('.details')
+    cy.get('h1.details-title').contains(nameText)
+    cy.get('.details-meta').contains('10178 Berlin')
+    cy.get('.details-meta').contains(urlText)
+    cy.get('.details-content').contains(descriptionText)
+  })
+})
