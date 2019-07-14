47fdd72d9db98ad7f8a171dba22645c78b7c039a
Christian Rijke <hello@christianrijke.de>
2019-07-14 22:14:31 +0200

    Fix: Disable CRA2 default code splitting for now, need to make this compatible with how we integrate this in Gatsby.

---
+++ map/craco.config.js
@@ -0,0 +1,30 @@
+module.exports = ({ env }) => {
+  if (env === 'production') {
+    return {
+      plugins: [
+        {
+          plugin: {
+            overrideWebpackConfig: ({ webpackConfig }) => {
+              // disable CRA 2 code splitting until we update the
+              // gatsby integration to support this
+
+              // eslint-disable-next-line no-param-reassign
+              webpackConfig.optimization.splitChunks = {
+                // chunks: 'all',
+                // name: false,
+                cacheGroups: {
+                  default: false
+                }
+              }
+              // eslint-disable-next-line no-param-reassign
+              webpackConfig.optimization.runtimeChunk = false
+              return webpackConfig
+            }
+          },
+          options: {}
+        }
+      ]
+    }
+  }
+  return {}
+}
