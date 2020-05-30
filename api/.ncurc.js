d33d5043fcb4c008110175ac6148050314f47b6b
Christian Rijke <hello@christianrijke.de>
2020-05-31 01:04:02 +0200

    Update packages and pin incompatible/deprecated packges.

---
+++ api/.ncurc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  upgrade: true,
+  reject: [
+    '@casl/ability',
+    '@feathersjs/authentication',
+    '@feathersjs/authentication-local',
+  ],
+}
---
+++ map/.ncurc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  upgrade: true,
+  reject: [
+    'react-router',
+    'leaflet',
+    'leaflet.markercluster',
+    'react-leaflet',
+    'react-leaflet-markercluster',
+    'react-router',
+    'susy',
+  ],
+}
