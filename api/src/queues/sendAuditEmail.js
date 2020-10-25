fda4dd482878862be4a17687e3430b4953938199
Christian Rijke <hello@christianrijke.de>
2020-10-25 13:26:34 +0100

    Send report to all admins.

--- api/src/queues/sendAuditEmail.js
+++ api/src/queues/sendAuditEmail.js
@@ -1,4 +1,5 @@
 import Queue from 'bull'
+import Role from '../models/roles'
 
 export const SEND_AUDIT_EMAIL = {
   queueName: 'send_audit_email',
@@ -12,15 +13,23 @@ export default (app) => {
 
   queue.process(async (job) => {
     job.log('sending audit email')
-    await app.service('admin/audit').find({
-      query: { email: 'true', recipient: app.get('mailer').message.from },
+    const adminRole = await Role.query()
+      .withGraphFetched('users')
+      .where({ name: 'admin' })
+    const admins = adminRole[0].users
+
+    admins.forEach((admin) => {
+      app.service('admin/audit').find({
+        query: { email: 'true', recipient: admin.email },
+      })
     })
+
     job.progress(100)
   })
 
   queue.add({ name: SEND_AUDIT_EMAIL.jobName })
   queue.add(
     { name: SEND_AUDIT_EMAIL.jobName },
-    { repeat: { cron: '0 0 16 ? * FRI *' } }
+    { repeat: { cron: '0 16 * * FRI' } }
   )
 }
