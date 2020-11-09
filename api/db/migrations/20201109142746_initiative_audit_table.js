834dc222d74972b5f2cc86b9231b7229917ef886
Christian Rijke <hello@christianrijke.de>
2020-11-09 14:37:47 +0100

    Add initiatives to email report.

---
+++ api/db/migrations/20201109142746_initiative_audit_table.js
@@ -0,0 +1,6 @@
+exports.up = async (knex) => {
+  await knex.raw(`
+create trigger initiatives_badges_audit after insert or update or delete on initiatives_badges for each row execute procedure if_modified_func();`)
+}
+
+exports.down = async (knex) => {}
--- api/src/services/admin/audit.js
+++ api/src/services/admin/audit.js
@@ -1,10 +1,17 @@
 import moment from 'moment'
 import Audit from '../../models/audit'
 
+const mapToText = rows => rows.map(
+  (d) =>
+    `${moment(d.time).format('DD.MM.YYYY')} - Betrieb ID ${d.id} ${
+      d.entityname
+    }: ${d.badgename} wurde entfernt`
+);
+
 export default (app) => {
   const service = {
     find: async (params) => {
-      const deletions = await Audit.knex().raw(
+      const farmDeletions = await Audit.knex().raw(
         `
         with farm_deletions as (
         select action_timestamp as time,
@@ -13,11 +20,11 @@ export default (app) => {
         from audit where action = 'd'
         and action_timestamp >= (current_timestamp at time zone 'utc' - interval '7 days')
         )
-        select d.time, f.id, f.name as farmname, b.name as badgename from farms f, badges b, farm_deletions d
+        select d.time, f.id, f.name as entityname, b.name as badgename from farms f, badges b, farm_deletions d
         where d.farm_id = f.id and b.id = d.badge_id
 `
       )
-      const insertions = await Audit.knex().raw(
+      const farmInsertions = await Audit.knex().raw(
         `
         with farm_insertions as (
         select action_timestamp as time,
@@ -26,24 +33,41 @@ export default (app) => {
         from audit where action = 'i'
         and action_timestamp >= (current_timestamp at time zone 'utc' - interval '7 days')
         )
-        select d.time, f.id, f.name as farmname, b.name as badgename from farms f, badges b, farm_insertions d
+        select d.time, f.id, f.name as entityname, b.name as badgename from farms f, badges b, farm_insertions d
         where d.farm_id = f.id and b.id = d.badge_id
 `
       )
 
+      const initiativeDeletions = await Audit.knex().raw(
+        `
+        with initiative_deletions as (
+        select action_timestamp as time,
+            svals(slice(old_values, ARRAY['initiative_id']))::INT as initiative_id,
+            svals(slice(old_values, ARRAY['badge_id']))::INT as badge_id
+        from audit where action = 'd'
+        and action_timestamp >= (current_timestamp at time zone 'utc' - interval '7 days')
+        )
+        select d.time, i.id, i.name as entityname, b.name as badgename from initiatives i, badges b, initiative_deletions d
+        where d.initiative_id = i.id and b.id = d.badge_id
+`
+      )
+      const initiativeInsertions = await Audit.knex().raw(
+        `
+        with initiative_insertions as (
+        select action_timestamp as time,
+            svals(slice(new_values, ARRAY['initiative_id']))::INT as initiative_id,
+            svals(slice(new_values, ARRAY['badge_id']))::INT as badge_id
+        from audit where action = 'i'
+        and action_timestamp >= (current_timestamp at time zone 'utc' - interval '7 days')
+        )
+        select d.time, i.id, i.name as entityname, b.name as badgename from initiatives i, badges b, initiative_insertions d
+          where d.initiative_id = i.id and b.id = d.badge_id
+`
+      )
+
       const report = {
-        deletions: deletions.rows.map(
-          (d) =>
-            `${moment(d.time).format('DD.MM.YYYY')} - Betrieb ID ${d.id} ${
-              d.farmname
-            }: ${d.badgename} wurde entfernt`
-        ),
-        insertions: insertions.rows.map(
-          (d) =>
-            `${moment(d.time).format('DD.MM.YYYY')} - Betrieb ID ${d.id} ${
-              d.farmname
-            }: ${d.badgename} wurde hinzugefügt`
-        ),
+        deletions: mapToText(farmDeletions.rows).concat(mapToText(initiativeDeletions.rows)),
+        insertions: mapToText(farmInsertions.rows).concat(mapToText(initiativeInsertions.rows))
       }
 
       if (params.query.email === 'true' && params.query.recipient) {
