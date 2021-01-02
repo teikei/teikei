c9282ddf9591487bbfdd3f1445eb6e2ee2b1c96d
Christian Rijke <hello@christianrijke.de>
2021-01-02 17:13:14 +0100

    Add separate configuration for audit recipients.

--- api/src/jobs/sendAuditEmail.js
+++ api/src/jobs/sendAuditEmail.js
@@ -1,18 +1,18 @@
-import Role from '../models/roles'
-
 export default (app) => {
   app.jobs.schedule('send audit email', '0 16 * * FRI', async () => {
     app.info('CRON: sending audit email - starting')
-    const adminRole = await Role.query()
-      .withGraphFetched('users')
-      .where({ name: 'admin' })
-    const admins = adminRole[0].users
 
-    admins.forEach((admin) => {
-      app.service('admin/audit').find({
-        query: { email: 'true', recipient: admin.email },
+    const recipients =
+      app.get('audit_recipients') & app.get('audit_recipients').split(',')
+    if (recipients) {
+      recipients.forEach((recipient) => {
+        app.service('admin/audit').find({
+          query: { email: 'true', recipient },
+        })
       })
-    })
+    } else {
+      app.info('CRON: no audit recipients specified, no audit email sent')
+    }
     app.info('CRON: sending audit email - done')
   })
 }
