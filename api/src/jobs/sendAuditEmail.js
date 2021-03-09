8b98a382fd7a365ae5d612b2b8ed2da2c0ac470f
Christian Rijke <hello@christianrijke.de>
2021-03-10 00:53:41 +0100

    Set cron back to Friday.

--- api/src/jobs/sendAuditEmail.js
+++ api/src/jobs/sendAuditEmail.js
@@ -1,5 +1,5 @@
 export default (app) => {
-  app.jobs.schedule('send audit email', '0 21 * * TUE', async () => {
+  app.jobs.schedule('send audit email', '0 16 * * FRI', async () => {
     app.info('CRON: sending audit email - starting')
 
     const mailerConfig = app.get('mailer')
