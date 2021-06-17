1e2771ba463a4fe362fea5a3d9eb584f836f801e
Christian Rijke <hello@christianrijke.de>
2021-06-17 19:32:02 +0200

    Read production config from env.

--- api/db/knexfile.js
+++ api/db/knexfile.js
@@ -1,12 +1,9 @@
 const devConfig = require('../config/default.json')
-const productionConfig = require('../config/production.json')
-
-const dotenv = require('dotenv')
-const path = require('path')
-
-dotenv.config({ path: path.resolve(__dirname, '..', '.env') })
 
 module.exports = {
   development: devConfig.postgres,
-  production: productionConfig.postgres,
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+  },
 }
