7cc987d0b7d93ea9fa1dd741f09a4b1f2a0c2714
Christian Rijke <hello@christianrijke.de>
2021-07-25 20:46:42 +0200

    Read api url from env variable.

--- admin/src/feathersClient.js
+++ admin/src/feathersClient.js
@@ -1,11 +1,11 @@
 import feathers from 'feathers-client'
 import superagent from 'superagent'
 
-const apiUrl = 'http://localhost:3030'
-
 const client = feathers()
 client.configure(feathers.hooks())
-client.configure(feathers.rest(apiUrl).superagent(superagent))
+client.configure(
+  feathers.rest(process.env.REACT_APP_API_URL).superagent(superagent)
+)
 client.configure(
   feathers.authentication({
     storage: window.localStorage,
