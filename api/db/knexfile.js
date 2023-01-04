9d65c7bd114721f6b498358ebae6c49e8c3787b8
Christian Rijke <hello@christianrijke.de>
2023-01-04 18:53:32 +0100

    Configure webtests in CI.

--- api/db/knexfile.js
+++ api/db/knexfile.js
@@ -2,6 +2,7 @@ const devConfig = require('../config/default.json')
 
 module.exports = {
   development: devConfig.postgres,
+  test: devConfig.postgres,
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
