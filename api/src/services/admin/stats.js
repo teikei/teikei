1b4745ef1db553dde44b11860a6e54a3ab792775
Christian Rijke <hello@christianrijke.de>
2023-09-20 21:02:18 +0200

    Fix entry count queries.

--- api/src/services/admin/stats.js
+++ api/src/services/admin/stats.js
@@ -3,20 +3,22 @@ import BaseModel from '../../models/base'
 export default (app) => {
   const service = {
     find: async () => {
-      const entryStats = await BaseModel.knex().raw(`
-      SELECT count(*) from farms
-      UNION
-      SELECT count(*) from depots
-      UNION
-      SELECT count(*) from initiatives
+      const depotCount = await BaseModel.knex().raw(`
+        SELECT count(*) from depots
+      `)
+      const farmCount = await BaseModel.knex().raw(`
+        SELECT count(*) from farms
+      `)
+      const initiativeCount = await BaseModel.knex().raw(`
+        SELECT count(*) from initiatives
       `)
       const userStats = await BaseModel.knex().raw(`
       SELECT state, count(*) from users group by state
       `)
       return [
-        { id: 1, resource: 'farms', stats: entryStats.rows[0] },
-        { id: 2, resource: 'depots', stats: entryStats.rows[1] },
-        { id: 3, resource: 'initiatives', stats: entryStats.rows[2] },
+        { id: 1, resource: 'farms', stats: farmCount.rows[0] },
+        { id: 2, resource: 'depots', stats: depotCount.rows[0] },
+        { id: 3, resource: 'initiatives', stats: initiativeCount.rows[0] },
         { id: 4, resource: 'users', stats: userStats.rows },
       ]
     },
