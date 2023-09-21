973e34a173fa7eab2b3600d09a439f04c82b2931
Christian Rijke <hello@christianrijke.de>
2023-09-21 12:04:56 +0200

    Import suppressions instead of bounces.

--- api/src/jobs/importEmailBounces.js
+++ api/src/jobs/importEmailBounces.js
@@ -4,47 +4,50 @@ import User from '../models/users'
 const JOB_NAME = 'import email bounces'
 const SCHEDULE_NIGHTLY_AT_1AM = '0 1 * * * '
 
-const POSTMARK_GET_BOUNCES_URL = 'https://api.postmarkapp.com/bounces'
+const POSTMARK_GET_SUPPRESSIONS_URL =
+  'https://api.postmarkapp.com/message-streams/broadcast/suppressions/dump'
 
-const getBouncesPage = async (token, page) => {
-  const bounces = await axios.get(
-    `${POSTMARK_GET_BOUNCES_URL}?type=HardBounce&inactive=true&count=100&offset=${
-      page * 100
-    }`,
-    {
+export default (app) => {
+  const suppressionNameMapping = {
+    HardBounce: 'Hard Bounce',
+    ManualSuppression: 'Unsubscribed',
+  }
+
+  const getNameForSuppressionReason = (suppression) =>
+    suppressionNameMapping[suppression] || 'unknown'
+
+  const importSuppressions = async (token) => {
+    const result = await axios.get(POSTMARK_GET_SUPPRESSIONS_URL, {
       headers: {
         'X-Postmark-Server-Token': token,
       },
+    })
+    if (
+      result === undefined ||
+      result.data === undefined ||
+      result.data.Suppressions === undefined ||
+      result.data.Suppressions.length === 0
+    ) {
+      app.info('no suppressions found to import')
+    } else {
+      await Promise.all(
+        result.data.Suppressions.map(
+          async ({ EmailAddress, SuppressionReason }) => {
+            const user = await User.query().where('email', EmailAddress)
+            if (user.length > 0) {
+              const userId = user[0].id
+              app.info(`recording suppression info on user ${userId}`)
+              app.service('users').patch(userId, {
+                bounce_type: SuppressionReason,
+                bounce_name: getNameForSuppressionReason(SuppressionReason),
+              })
+            } else {
+              app.info(`user ${EmailAddress} not found`)
+            }
+          }
+        )
+      )
     }
-  )
-  return bounces.data
-}
-
-export default (app) => {
-  const importBounces = async (token) => {
-    const allBounces = []
-    let currentPage
-    let page = 0
-    do {
-      currentPage = await getBouncesPage(token, page)
-      allBounces.push(...currentPage.Bounces)
-      page++
-    } while (currentPage.Bounces.length > 0)
-    await Promise.all(
-      allBounces.map(async ({ Email, Type, Name }) => {
-        const user = await User.query().where('email', Email)
-        if (user.length > 0) {
-          const userId = user[0].id
-          app.info(`recording bounce info on user ${userId}`)
-          app.service('users').patch(userId, {
-            bounce_type: Type,
-            bounce_name: Name,
-          })
-        } else {
-          app.info(`user ${Email} not found`)
-        }
-      })
-    )
   }
 
   app.jobs.schedule(8, JOB_NAME, SCHEDULE_NIGHTLY_AT_1AM, async () => {
@@ -53,11 +56,11 @@ export default (app) => {
     if (app.isProduction() && transport === 'postmarkTransport') {
       app.info('importing bounces from Postmark Production server')
       const token = app.get('mailer').postmarkTransport.auth.apiKey
-      await importBounces(token)
-    } else if (app.isProduction() && transport === 'postmarkSandboxTransport') {
+      await importSuppressions(token)
+    } else if (transport === 'postmarkSandboxTransport') {
       app.info('importing bounces from Postmark Sandbox server')
       const token = app.get('mailer').postmarkSandboxTransport.auth.apiKey
-      await importBounces(token)
+      await importSuppressions(token)
     } else {
       app.info(
         `transport ${transport} is not a valid Postmark transport. Skipping import.`
