6acbf31a0cd41bd12997c818017600dbc22cde58
Christian Rijke <hello@christianrijke.de>
2023-10-31 10:35:12 +0100

    update deploy script (#478)

--- deploy_all.sh
+++ deploy_all.sh
@@ -2,6 +2,6 @@
 git push dokku-teikei-map
 git push dokku-teikei-api
 git push dokku-teikei-admin
-git push dokku-teikei-map-preview preview:master
-git push dokku-teikei-api-preview preview:master
-git push dokku-teikei-admin-preview preview:master
+git push dokku-teikei-map-preview preview:main
+git push dokku-teikei-api-preview preview:main
+git push dokku-teikei-admin-preview preview:main
