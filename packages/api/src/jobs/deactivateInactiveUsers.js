0ba9b46c3340b7eecb859b16eff83356cc5d8fc5
Christian Rijke <hello@christianrijke.de>
2023-11-25 15:37:42 +0100

    Don't automatically deactivate entries of inactive users. (#483)

--- packages/api/src/jobs/deactivateInactiveUsers.js
+++ packages/api/src/jobs/deactivateInactiveUsers.js
@@ -1,32 +1,5 @@
 import BaseModel from '../models/base'
 
-const deactivateEntries = async () => {
-  await BaseModel.knex().raw(`
-    update farms f set active = false where f.id IN
-    (select fa.farm_id from farms_users fa, users u
-    where u.id = fa.user_id
-    and u.state = 'ACTIVE_SECOND_REMINDER_SENT'
-    and u.reminder_sent_at <= current_date - interval '8 weeks')
-  `)
-
-  await BaseModel.knex().raw(`
-    update initiatives i set active = false where i.id IN
-    (select ia.initiative_id from initiatives_users ia, users u
-    where u.id = ia.user_id
-    and u.state = 'ACTIVE_SECOND_REMINDER_SENT'
-    and u.reminder_sent_at <= current_date - interval '8 weeks')
-  `)
-
-  await BaseModel.knex().raw(`
-    update depots d set active = false where d.id IN
-    (select fd.depot_id from farms_users fa, users u, farms_depots fd
-    where u.id = fa.user_id
-    and fd.farm_id = fa.farm_id
-    and u.state = 'ACTIVE_SECOND_REMINDER_SENT'
-    and u.reminder_sent_at <= current_date - interval '8 weeks')
-  `)
-}
-
 const deactivateUsers = async () => {
   await BaseModel.knex().raw(
     `update users
@@ -44,8 +17,6 @@ export default (app) => {
   app.jobs.schedule(7, JOB_NAME, EVERY_MONDAY_AT_6, async () => {
     app.info(`CRON: ${JOB_NAME} - starting`)
 
-    app.info('deactivating entries')
-    await deactivateEntries()
     app.info('deactivating users')
     await deactivateUsers()
 
