62f40916e238ea1b3a91634a4c3d5ded08a025fb
Christian Rijke <hello@christianrijke.de>
2023-12-31 17:38:11 +0100

    Update map dependencies.

--- packages/map/.ncurc.js
+++ packages/map/.ncurc.js
@@ -1,12 +1,4 @@
 module.exports = {
   upgrade: true,
-  reject: [
-    'react-select',
-    'history',
-    'react-leaflet',
-    'mapbox-gl',
-    'react-router',
-    'react-router-dom',
-    'feathers-authentication-management',
-  ],
+  reject: ['react-select', 'react-leaflet', 'mapbox-gl'],
 }
