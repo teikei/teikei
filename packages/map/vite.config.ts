6795fa0af318de9f597e4387cd3a30a5c38680a0
Christian Rijke <hello@christianrijke.de>
2024-04-16 22:10:45 +0200

    feat(vite): Fix webtests, define 'global' for legacy libs.

--- packages/map/vite.config.ts
+++ packages/map/vite.config.ts
@@ -7,4 +7,11 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis',
+      },
+    },
+  },
 })
