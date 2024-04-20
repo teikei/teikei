219ad17792026488fbbd22f065c1a38778000340
Christian Rijke <hello@christianrijke.de>
2024-04-20 12:17:04 +0200

    fix deployment.

--- packages/map/vite.config.ts
+++ packages/map/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { PreRenderedAsset } from 'rollup'
 
 export default defineConfig({
   plugins: [react()],
@@ -8,21 +7,7 @@ export default defineConfig({
     port: 3000
   },
   build: {
-    lib: {
-      entry: './src/main.tsx',
-      formats: ['cjs', 'es'],
-      fileName: (format) => (format === 'cjs' ? 'main.js' : 'main.esm.js')
-    },
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo: PreRenderedAsset) => {
-          if (assetInfo.name === 'style.css') {
-            return 'main.css'
-          }
-          return assetInfo.name ?? ''
-        }
-      }
-    }
+    target: 'es2015'
   },
   optimizeDeps: {
     esbuildOptions: {
