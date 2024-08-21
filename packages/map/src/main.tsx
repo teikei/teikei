d1cefd259eab4ae43a137595b05c2da8a23e051f
Christian Rijke <hello@christianrijke.de>
2024-08-21 20:59:57 +0200

    i18n: Setup i18next-react.

--- packages/map/src/main.tsx
+++ packages/map/src/main.tsx
@@ -1,4 +1,8 @@
+// @ts-ignore
 import AuthManagement from 'feathers-authentication-management/src/client'
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import resourcesToBackend from 'i18next-resources-to-backend'
 
 import 'leaflet/dist/leaflet.css'
 import 'leaflet.markercluster/dist/MarkerCluster.css'
@@ -20,6 +24,27 @@ const configDataset = {
 export const config = makeConfiguration(configDataset)
 export const client = makeClient(config.apiBaseUrl)
 export const authManagement = new AuthManagement(client)
+
+const defaultLocale =
+  import.meta.env.NODE_ENV === 'development' ? 'dev' : 'de-DE'
+
+i18n
+  .use(
+    resourcesToBackend(
+      (language: string, namespace: string) =>
+        import(`./locales/${language}/${namespace}.json`)
+    )
+  )
+  .use(initReactI18next)
+  .init({
+    lng: defaultLocale,
+    fallbackLng: defaultLocale,
+    interpolation: {
+      escapeValue: false
+    },
+    ns: ['common', 'map', 'search'],
+    defaultNS: ['common']
+  })
 
 if (appContainerEl) {
   render(config, appContainerEl, makeMap)
