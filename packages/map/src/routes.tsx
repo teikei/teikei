5e4698a2b779c5fa2e2dacf2ae456e50b252411c
Christian Rijke <hello@christianrijke.de>
2024-10-14 22:49:34 +0200

    Disable lazy loading.

--- packages/map/src/routes.tsx
+++ packages/map/src/routes.tsx
@@ -1,5 +1,27 @@
 import { useCallback } from 'react'
 
+// TODO code splitting and lazy loading
+import * as Root from './root'
+import * as CreateDepot from './routes/places/create-depot'
+import * as CreateFarm from './routes/places/create-farm'
+import * as CreateInitiative from './routes/places/create-initiative'
+import * as EditDepot from './routes/places/edit-depot'
+import * as EditFarm from './routes/places/edit-farm'
+import * as EditInitiative from './routes/places/edit-initiative'
+import * as DeleteDepot from './routes/places/delete-depot'
+import * as DeleteFarm from './routes/places/delete-farm'
+import * as DeleteInitiative from './routes/places/delete-initiative'
+import * as EditAccount from './routes/users/edit-account'
+import * as EditPassword from './routes/users/edit-password'
+import * as RecoverPassword from './routes/users/recover-password'
+import * as ResetPassword from './routes/users/reset-password'
+import * as MyEntries from './routes/places/myentries'
+import * as MapPosition from './routes/map/map-position'
+import * as MapPlaceDetail from './routes/map/map-place-detail'
+import * as Map from './routes/map/map'
+import * as SignIn from './routes/users/signin'
+import * as SignUp from './routes/users/signup'
+
 export const MAP = '/'
 export const SHOW_PLACE = '/:type/:id'
 export const SHOW_POSITION = '/position/:lat,:lon'
@@ -62,82 +84,96 @@ export const useQueryString = () => {
 export default function getRoutes() {
   const routes = [
     {
-      lazy: () => import('./root'),
+      element: <Root.Component />,
+      loader: Root.loader,
       id: 'root',
       children: [
         {
           path: NEW_DEPOT,
-          lazy: () => import('./routes/places/create-depot')
+          element: <CreateDepot.Component />,
+          loader: CreateDepot.loader
         },
         {
           path: NEW_FARM,
-          lazy: () => import('./routes/places/create-farm')
+          element: <CreateFarm.Component />,
+          loader: CreateFarm.loader
         },
         {
           path: NEW_INITIATIVE,
-          lazy: () => import('./routes/places/create-initiative')
+          element: <CreateInitiative.Component />,
+          loader: CreateInitiative.loader
         },
         {
           path: EDIT_DEPOT,
-          lazy: () => import('./routes/places/edit-depot')
+          element: <EditDepot.Component />,
+          loader: EditDepot.loader
         },
         {
           path: EDIT_FARM,
-          lazy: () => import('./routes/places/edit-farm')
+          element: <EditFarm.Component />,
+          loader: EditFarm.loader
         },
         {
           path: EDIT_INITIATIVE,
-          lazy: () => import('./routes/places/edit-initiative')
+          element: <EditInitiative.Component />,
+          loader: EditInitiative.loader
         },
         {
           path: DELETE_DEPOT,
-          lazy: () => import('./routes/places/delete-depot')
+          element: <DeleteDepot.Component />,
+          loader: DeleteDepot.loader
         },
         {
           path: DELETE_FARM,
-          lazy: () => import('./routes/places/delete-farm')
+          element: <DeleteFarm.Component />,
+          loader: DeleteFarm.loader
         },
         {
           path: DELETE_INITIATIVE,
-          lazy: () => import('./routes/places/delete-initiative')
+          element: <DeleteInitiative.Component />,
+          loader: DeleteInitiative.loader
         },
         {
           path: EDIT_USER_ACCOUNT,
-          lazy: () => import('./routes/users/edit-account')
+          element: <EditAccount.Component />
         },
         {
           path: EDIT_USER_PASSWORD,
-          lazy: () => import('./routes/users/edit-password')
+          element: <EditPassword.Component />
         },
         {
           path: RECOVER_PASSWORD,
-          lazy: () => import('./routes/users/recover-password')
+          element: <RecoverPassword.Component />
         },
         {
           path: RESET_PASSWORD,
-          lazy: () => import('./routes/users/reset-password')
+          element: <ResetPassword.Component />
         },
         {
           path: MY_ENTRIES,
-          lazy: () => import('./routes/places/myentries')
+          element: <MyEntries.Component />,
+          loader: MyEntries.loader
         },
         {
           path: SHOW_POSITION,
-          lazy: () => import('./routes/map/map-position')
+          element: <MapPosition.Component />,
+          loader: MapPosition.loader
         },
         {
           path: SHOW_PLACE,
-          lazy: () => import('./routes/map/map-place-detail')
+          element: <MapPlaceDetail.Component />,
+          loader: MapPlaceDetail.loader
         },
-        { path: MAP, lazy: () => import('./routes/map/map') }
+        { path: MAP, element: <Map.Component />, loader: Map.loader }
       ]
     },
     {
       path: '/',
-      lazy: () => import('./root'),
+      element: Root.Component,
+      loader: Root.loader,
       children: [
-        { path: SIGN_IN, lazy: () => import('./routes/users/signin') }, // TODO these routes are not protected
-        { path: SIGN_UP, lazy: () => import('./routes/users/signup') } //  TODO these routes are not protected
+        { path: SIGN_IN, element: <SignIn.Component /> }, // TODO these routes are not protected
+        { path: SIGN_UP, element: <SignUp.Component /> } //  TODO these routes are not protected
       ]
     }
   ]
