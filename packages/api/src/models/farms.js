bace2031f1ee8a730ca8bb15e75e5ce5e9b8a8cf
Christian Rijke <hello@christianrijke.de>
2025-05-10 21:45:48 +0200

    bug(hasBadge): Make sure hasOrigin modifier does not overwrite the hasBadge and notHasBadge modifier.

--- packages/api/src/models/farms.js
+++ packages/api/src/models/farms.js
@@ -72,20 +72,32 @@ export class FarmAdmin extends Farm {
 
   static get modifiers() {
     return {
-      hasBadge: function (builder, badgeId) {
+      hasBadge: function (builder, badgeId, origins) {
         builder.whereExists(function () {
           this.select('*')
             .from('farms_badges')
             .whereRaw('farms_badges.farm_id = farms.id')
             .whereRaw(`farms_badges.badge_id = ${badgeId}`)
+            .whereExists(function () {
+              this.select('*')
+                .from('farms_origins')
+                .whereRaw('farms_origins.farm_id = farms.id')
+                .whereIn('farms_origins.origin', origins)
+            })
         })
       },
-      notHasBadge: function (builder, badgeId) {
+      notHasBadge: function (builder, badgeId, origins) {
         builder.whereNotExists(function () {
           this.select('*')
             .from('farms_badges')
             .whereRaw('farms_badges.farm_id = farms.id')
             .whereRaw(`farms_badges.badge_id = ${badgeId}`)
+            .whereExists(function () {
+              this.select('*')
+                .from('farms_origins')
+                .whereRaw('farms_origins.farm_id = farms.id')
+                .whereIn('farms_origins.origin', origins)
+            })
         })
       },
       hasOrigin: function (builder, origins) {
