1541160ce7b70fff0fcdfd2340defb3e1a3be086
Christian Rijke <hello@christianrijke.de>
2025-05-11 16:33:23 +0200

    bug(hasBadge): Rewrite query without inner whereExists. (#684)
    * bug(hasBadge): Make sure hasOrigin modifier does not overwrite the hasBadge and notHasBadge modifier.
    
    * bug(hasBadge): Rewrite query without inner whereExists.
    
    * bug(hasBadge): Make sure hasOrigin modifier does not overwrite the hasBadge and notHasBadge modifier. (#682)
    
    * bug(hasBadge): Rewrite query without inner whereExists.
    
    * bug(hasBadge): Make sure hasOrigin modifier does not overwrite the hasBadge and notHasBadge modifier. (#682)
    
    # Conflicts:
    #	packages/api/src/models/farms.js
    
    * bug(hasBadge): Rewrite query without inner whereExists.

--- packages/api/src/models/farms.js
+++ packages/api/src/models/farms.js
@@ -73,32 +73,34 @@ export class FarmAdmin extends Farm {
   static get modifiers() {
     return {
       hasBadge: function (builder, badgeId, origins) {
-        builder.whereExists(function () {
-          this.select('*')
-            .from('farms_badges')
-            .join(
-              'farms_origins',
-              'farms_badges.farm_id',
-              'farms_origins.farm_id'
-            )
-            .whereRaw('farms_badges.farm_id = farms.id')
-            .whereRaw(`farms_badges.badge_id = ${badgeId}`)
-            .whereIn('farms_origins.origin', origins)
-        })
+        builder
+          .whereExists(function () {
+            this.select('*')
+              .from('farms_origins')
+              .whereRaw('farms_origins.farm_id = farms.id')
+              .whereIn('farms_origins.origin', origins)
+          })
+          .whereExists(function () {
+            this.select('*')
+              .from('farms_badges')
+              .whereRaw('farms_badges.farm_id = farms.id')
+              .whereRaw(`farms_badges.badge_id = ${badgeId}`)
+          })
       },
       notHasBadge: function (builder, badgeId, origins) {
-        builder.whereNotExists(function () {
-          this.select('*')
-            .from('farms_badges')
-            .join(
-              'farms_origins',
-              'farms_badges.farm_id',
-              'farms_origins.farm_id'
-            )
-            .whereRaw('farms_badges.farm_id = farms.id')
-            .whereRaw(`farms_badges.badge_id = ${badgeId}`)
-            .whereIn('farms_origins.origin', origins)
-        })
+        builder
+          .whereExists(function () {
+            this.select('*')
+              .from('farms_origins')
+              .whereRaw('farms_origins.farm_id = farms.id')
+              .whereIn('farms_origins.origin', origins)
+          })
+          .whereNotExists(function () {
+            this.select('*')
+              .from('farms_badges')
+              .whereRaw('farms_badges.farm_id = farms.id')
+              .whereRaw(`farms_badges.badge_id = ${badgeId}`)
+          })
       },
       hasOrigin: function (builder, origins) {
         builder.whereExists(function () {
