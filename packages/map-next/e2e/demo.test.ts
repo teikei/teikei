121993fb5a919ec09be46d8c7f65b2c6d55402c4
Christian Rijke <hello@christianrijke.de>
2025-12-20 22:22:02 +0100

    chore: setup proper unit and e2e tests.  (#740)

--- packages/map-next/e2e/demo.test.ts
+++ packages/map-next/e2e/demo.test.ts
@@ -2,5 +2,6 @@ import { expect, test } from '@playwright/test';
 
 test('home page has expected h1', async ({ page }) => {
 	await page.goto('/');
+	// TODO this test only works because the error page also has an h1
 	await expect(page.locator('h1')).toBeVisible();
 });
--- packages/map-next/vite.config.ts
+++ packages/map-next/vite.config.ts
@@ -2,6 +2,7 @@ import { paraglideVitePlugin } from '@inlang/paraglide-js';
 import devtoolsJson from 'vite-plugin-devtools-json';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vitest/config';
+import { playwright } from '@vitest/browser-playwright';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
@@ -9,23 +10,41 @@ export default defineConfig({
 		tailwindcss(),
 		sveltekit(),
 		devtoolsJson(),
-		paraglideVitePlugin({
-			project: './project.inlang',
-			outdir: './src/lib/paraglide'
-		})
+		paraglideVitePlugin({ project: './project.inlang', outdir: './src/lib/paraglide' })
 	],
 	server: { port: 3000 },
+
 	test: {
 		expect: { requireAssertions: true },
-		// Pure client-side testing only
-		name: 'client',
-		environment: 'browser',
-		browser: {
-			enabled: true,
-			provider: 'playwright',
-			instances: [{ browser: 'chromium' }]
-		},
-		include: ['src/**/*.{test,spec}.{js,ts}'],
-		setupFiles: ['./vitest-setup-client.ts']
+
+		projects: [
+			{
+				extends: './vite.config.ts',
+
+				test: {
+					name: 'client',
+
+					browser: {
+						enabled: true,
+						provider: playwright(),
+						instances: [{ browser: 'chromium', headless: true }]
+					},
+
+					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
+					exclude: ['src/lib/server/**']
+				}
+			},
+
+			{
+				extends: './vite.config.ts',
+
+				test: {
+					name: 'server',
+					environment: 'node',
+					include: ['src/**/*.{test,spec}.{js,ts}'],
+					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
+				}
+			}
+		]
 	}
 });
