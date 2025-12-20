81c74fda721c2c027020609d79aebc9a55728202
Christian Rijke <hello@christianrijke.de>
2025-12-20 20:02:37 +0100

    add scripts and dokku config.

--- packages/map-next/svelte.config.js
+++ packages/map-next/svelte.config.js
@@ -3,14 +3,15 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
 	kit: {
-		adapter: adapter({
-			fallback: 'index.html'
-		})
+		adapter: adapter({ fallback: 'index.html' }),
+		router: {
+			type: 'hash'
+		},
+		output: {
+			bundleStrategy: 'single'
+		}
 	}
 };
 
