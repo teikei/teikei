47ed03e5f1f8fde8cc01700f593d2a264d472202
Christian Rijke <hello@christianrijke.de>
2026-01-18 19:32:47 +0100

    fix(validation): allow empty values for country, state, and postalcode fields

--- packages/api/src/models/validation/joi/entry.js
+++ packages/api/src/models/validation/joi/entry.js
@@ -5,11 +5,11 @@ export const entry = {
   type: Joi.string(), // server only
   link: Joi.string(), // server only
   name: Joi.string().max(255).trim().required(),
-  country: Joi.string().max(255).trim(),
-  state: Joi.string().max(255).trim(),
-  street: Joi.string().max(255).trim(),
-  housenumber: Joi.string().max(255).trim(),
-  postalcode: Joi.string().max(255).trim(),
+  country: Joi.string().max(255).trim().allow(''),
+  state: Joi.string().max(255).trim().allow(''),
+  street: Joi.string().max(255).trim().allow('').allow(null),
+  housenumber: Joi.string().max(255).trim().allow(''),
+  postalcode: Joi.string().max(255).trim().allow(''),
   city: Joi.string().max(255).trim().required(),
   latitude: Joi.number().required(),
   longitude: Joi.number().required()
