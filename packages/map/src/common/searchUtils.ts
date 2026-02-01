98dc0f7def600aea68c6e80b4949230856801739
Christian Rijke <hello@christianrijke.de>
2026-02-01 16:44:05 +0100

    fix: remove cityOf function and update GeocoderSearchField to directly use city

--- packages/map/src/common/searchUtils.ts
+++ packages/map/src/common/searchUtils.ts
@@ -9,7 +9,5 @@ export interface AddressItem {
 export const addressOf = ({ street, houseNumber }: AddressItem) =>
   [street, houseNumber].join(' ').trim()
 
-export const cityOf = ({ city }: AddressItem) => (city ?? '').trim()
-
-export const labelOf = (item: AddressItem) =>
-  _.compact([addressOf(item), cityOf(item)]).join(', ')
+export const labelOf = ({ street, houseNumber, city }: AddressItem) =>
+  _.compact([addressOf({ street, houseNumber }), city]).join(', ')
