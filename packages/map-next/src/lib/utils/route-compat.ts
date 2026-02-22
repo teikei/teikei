708182e9e03cf931661ea21b98e523d9b65d16e7
Christian Rijke <hello@christianrijke.de>
2026-02-22 23:18:35 +0100

    feat: open associated farm detail view when clicking a depot map entry (#790)
    * feat: open associated farm detail view when clicking a depot map entry
    
    * feat: implement getDepotAssociatedFarmId and update related imports in places and routes
    
    * feat: update sidebarEntries to use mapEntries for filtering features

--- packages/map-next/src/lib/utils/route-compat.ts
+++ packages/map-next/src/lib/utils/route-compat.ts
@@ -1,4 +1,4 @@
-import { getDepotAssociatedFarmId } from '$lib/api/places';
+import { getDepotAssociatedFarmId } from '$lib/utils/places';
 import { parseHashRoute, routeBuilders } from '$lib/utils/routes';
 
 /**
