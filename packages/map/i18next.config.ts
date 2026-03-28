79bd1b1192ec2e7060fcb49db419983236aa6730
Christian Rijke <hello@christianrijke.de>
2026-03-28 17:16:55 +0100

    Replace i18next-parser with i18next-cli in legacy map

---
+++ packages/map/i18next.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'i18next-cli'
+
+export default defineConfig({
+  locales: ['de-CH', 'de-DE', 'fr-CH'],
+  extract: {
+    input: ['src/**/*.{js,jsx,ts,tsx}'],
+    output: 'src/locales/{{language}}/{{namespace}}.json',
+    defaultNS: 'common',
+    defaultValue: '',
+    keySeparator: false,
+    nsSeparator: ':',
+    contextSeparator: '_',
+    pluralSeparator: '_',
+    functions: ['t', '*.t'],
+    transComponents: ['Trans'],
+    preservePatterns: [
+      'common:months.*',
+      'common:forms.labels.goals.*',
+      'common:products.*',
+      'common:productcategories.*',
+      'common:badgescategories.*',
+      'validations:*'
+    ],
+    sort: true,
+    indentation: 2,
+    primaryLanguage: 'de-DE',
+    secondaryLanguages: ['de-CH', 'fr-CH']
+  }
+})
