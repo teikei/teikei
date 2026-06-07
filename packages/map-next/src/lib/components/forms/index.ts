9ec75f56353e46a42f9bd77c041336c5cb5ae6cc
Christian Rijke <hello@christianrijke.de>
2026-06-07 16:32:01 +0200

    feat: Enhance form components by adding FormTextarea and updating FormSelect structure

---
+++ packages/map-next/src/lib/components/forms/index.ts
@@ -0,0 +1,7 @@
+import FormErrorAlert from './FormErrorAlert.svelte';
+import FormInput, { type FormInputProps } from './FormInput.svelte';
+import FormSelect from './FormSelect.svelte';
+import FormTextarea, { type FormTextareaProps } from './FormTextarea.svelte';
+
+export { FormErrorAlert, FormInput, FormSelect, FormTextarea };
+export type { FormInputProps, FormTextareaProps };
